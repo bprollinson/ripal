2986f72b983915785f30a995c4f770c48a744fcd
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-11 22:46:21 -0500

    Adding handling of brackets in parser

--- RegularExpressionSyntaxParserTest.java
+++ RegularExpressionSyntaxParserTest.java
@@ -3,9 +3,11 @@ import org.junit.Test;
 
 import larp.grammar.CharacterNode;
 import larp.grammar.CharacterToken;
+import larp.grammar.CloseBraceToken;
 import larp.grammar.ConcatenationNode;
 import larp.grammar.KleeneClosureNode;
 import larp.grammar.KleeneClosureToken;
+import larp.grammar.OpenBraceToken;
 import larp.grammar.RegularExpressionSyntaxNode;
 import larp.grammar.RegularExpressionSyntaxParser;
 import larp.grammar.RegularExpressionSyntaxToken;
@@ -62,6 +64,29 @@ public class RegularExpressionSyntaxParserTest
         KleeneClosureNode kleeneClosureNode = new KleeneClosureNode();
         kleeneClosureNode.addChild(new CharacterNode('b'));
         expectedRootNode.addChild(kleeneClosureNode);
+
+        assertEquals(expectedRootNode, rootNode);
+    }
+
+    @Test
+    public void testParseHandlesBraces()
+    {
+        RegularExpressionSyntaxParser parser = new RegularExpressionSyntaxParser();
+
+        Vector<RegularExpressionSyntaxToken> input = new Vector<RegularExpressionSyntaxToken>();
+        input.add(new CharacterToken('a'));
+        input.add(new OpenBraceToken());
+        input.add(new CharacterToken('b'));
+        input.add(new CloseBraceToken());
+        input.add(new CharacterToken('c'));
+        RegularExpressionSyntaxNode rootNode = parser.parse(input);
+
+        ConcatenationNode expectedRootNode = new ConcatenationNode();
+        expectedRootNode.addChild(new CharacterNode('a'));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new CharacterNode('b'));
+        expectedRootNode.addChild(concatenationNode);
+        expectedRootNode.addChild(new CharacterNode('c'));
 
         assertEquals(expectedRootNode, rootNode);
     }
--- larp/grammar/RegularExpressionSyntaxParser.java
+++ larp/grammar/RegularExpressionSyntaxParser.java
@@ -7,21 +7,39 @@ public class RegularExpressionSyntaxParser
     public RegularExpressionSyntaxNode parse(Vector<RegularExpressionSyntaxToken> tokens)
     {
         ConcatenationNode node = new ConcatenationNode();
+        int braceBalance = 0;
+        int openBracePosition = -1;
+
         for (int i = 0; i < tokens.size(); i++)
         {
             RegularExpressionSyntaxToken token = tokens.get(i);
 
-            if (token instanceof CharacterToken)
+            if (token instanceof CharacterToken && braceBalance == 0)
             {
                 node.addChild(new CharacterNode(((CharacterToken)token).getCharacter()));
             }
-            else if (token instanceof KleeneClosureToken)
+            else if (token instanceof KleeneClosureToken && braceBalance == 0)
             {
                 Vector<RegularExpressionSyntaxNode> childNodes = node.getChildNodes();
                 RegularExpressionSyntaxNode lastNode = childNodes.lastElement();
                 KleeneClosureNode kleeneClosureNode = new KleeneClosureNode();
                 kleeneClosureNode.addChild(lastNode);
                 childNodes.set(childNodes.size() - 1, kleeneClosureNode);
+            }
+            else if (token instanceof OpenBraceToken)
+            {
+                braceBalance++;
+                openBracePosition = i;
+            }
+            else if (token instanceof CloseBraceToken)
+            {
+                braceBalance--;
+
+                if (braceBalance == 0)
+                {
+                    node.addChild(this.parse(new Vector(tokens.subList(openBracePosition + 1, i))));
+                    openBracePosition = -1;
+                }
             }
         }
 
