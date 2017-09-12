b80de4d2a3dde744046c2d6e904da328c9e07d64
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-12 18:04:38 -0500

    Adding some stepwise refinement to regular expression syntax parser

--- larp/grammar/RegularExpressionSyntaxParser.java
+++ larp/grammar/RegularExpressionSyntaxParser.java
@@ -6,6 +6,54 @@ public class RegularExpressionSyntaxParser
 {
     public RegularExpressionSyntaxNode parse(Vector<RegularExpressionSyntaxToken> tokens)
     {
+        RegularExpressionSyntaxNode orNode = this.parseOrExpression(tokens);
+        if (orNode != null)
+        {
+            return orNode;
+        }
+
+        ConcatenationNode node = new ConcatenationNode();
+        int braceBalance = 0;
+        int openBracePosition = -1;
+
+        for (int i = 0; i < tokens.size(); i++)
+        {
+            RegularExpressionSyntaxToken token = tokens.get(i);
+
+            if (token instanceof CharacterToken && braceBalance == 0)
+            {
+                node.addChild(new CharacterNode(((CharacterToken)token).getCharacter()));
+            }
+            else if (token instanceof KleeneClosureToken && braceBalance == 0)
+            {
+                Vector<RegularExpressionSyntaxNode> childNodes = node.getChildNodes();
+                RegularExpressionSyntaxNode lastNode = childNodes.lastElement();
+                KleeneClosureNode kleeneClosureNode = new KleeneClosureNode();
+                kleeneClosureNode.addChild(lastNode);
+                childNodes.set(childNodes.size() - 1, kleeneClosureNode);
+            }
+            else if (token instanceof OpenBraceToken)
+            {
+                braceBalance++;
+                openBracePosition = i;
+            }
+            else if (token instanceof CloseBraceToken)
+            {
+                braceBalance--;
+
+                if (braceBalance == 0)
+                {
+                    node.addChild(this.parse(new Vector(tokens.subList(openBracePosition + 1, i))));
+                    openBracePosition = -1;
+                }
+            }
+        }
+
+        return node;
+    }
+
+    private RegularExpressionSyntaxNode parseOrExpression(Vector<RegularExpressionSyntaxToken> tokens)
+    {
         ConcatenationNode node = new ConcatenationNode();
         int braceBalance = 0;
         int openBracePosition = -1;
@@ -48,39 +96,6 @@ public class RegularExpressionSyntaxParser
             return node;
         }
 
-        for (int i = 0; i < tokens.size(); i++)
-        {
-            RegularExpressionSyntaxToken token = tokens.get(i);
-
-            if (token instanceof CharacterToken && braceBalance == 0)
-            {
-                node.addChild(new CharacterNode(((CharacterToken)token).getCharacter()));
-            }
-            else if (token instanceof KleeneClosureToken && braceBalance == 0)
-            {
-                Vector<RegularExpressionSyntaxNode> childNodes = node.getChildNodes();
-                RegularExpressionSyntaxNode lastNode = childNodes.lastElement();
-                KleeneClosureNode kleeneClosureNode = new KleeneClosureNode();
-                kleeneClosureNode.addChild(lastNode);
-                childNodes.set(childNodes.size() - 1, kleeneClosureNode);
-            }
-            else if (token instanceof OpenBraceToken)
-            {
-                braceBalance++;
-                openBracePosition = i;
-            }
-            else if (token instanceof CloseBraceToken)
-            {
-                braceBalance--;
-
-                if (braceBalance == 0)
-                {
-                    node.addChild(this.parse(new Vector(tokens.subList(openBracePosition + 1, i))));
-                    openBracePosition = -1;
-                }
-            }
-        }
-
-        return node;
+        return null;
     }
 }
