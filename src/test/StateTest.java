0bd605768fc5ab2608ac6f903079f668d7040f6d
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-17 09:26:13 -0500

    Unit test for transition characters in different order

--- src/test/StateTest.java
+++ src/test/StateTest.java
@@ -47,11 +47,22 @@ public class StateTest
     public void testEqualsReturnsTrueForSameTransitionChracters()
     {
         State state = new TestState("S0", true);
-        state.addTransition(new StateTransition('a', state));
-        State otherState = new TestState("S1", true);
-        otherState.addTransition(new StateTransition('a', otherState));
+        state.addTransition(new StateTransition('a', new TestState("S1", true)));
+        State otherState = new TestState("S2", true);
+        otherState.addTransition(new StateTransition('a', new TestState("S3", true)));
 
         assertTrue(state.equals(otherState));
+    }
+
+    @Test
+    public void testEqualsReturnsTrueForSameTransitionCharactersInDifferentOrder()
+    {
+        State state = new TestState("S0", true);
+        state.addTransition(new StateTransition('a', new TestState("S1", true)));
+        state.addTransition(new StateTransition('b', new TestState("S2", true)));
+        State otherState = new TestState("S3", true);
+        otherState.addTransition(new StateTransition('b', new TestState("S4", true)));
+        otherState.addTransition(new StateTransition('a', new TestState("S5", true)));
     }
 
     private class TestState extends State
