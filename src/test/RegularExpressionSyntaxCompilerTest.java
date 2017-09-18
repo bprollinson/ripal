22630b470737fe61654cfbe4c6d9c5226d66c9ff
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-18 06:09:59 -0500

    Test for concatenation epsilon NFA construction

--- src/test/RegularExpressionSyntaxCompilerTest.java
+++ src/test/RegularExpressionSyntaxCompilerTest.java
@@ -6,6 +6,7 @@ import larp.automaton.EpsilonNFAState;
 import larp.automaton.RegularExpressionSyntaxCompiler;
 import larp.automaton.StateTransition;
 import larp.grammar.CharacterNode;
+import larp.grammar.ConcatenationNode;
 import larp.grammar.KleeneClosureNode;
 
 public class RegularExpressionSyntaxCompilerTest
@@ -35,6 +36,27 @@ public class RegularExpressionSyntaxCompilerTest
         state1.addTransition(new StateTransition('a', state2));
         state1.addTransition(new StateTransition(null, state2));
         state2.addTransition(new StateTransition(null, state1));
+        EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state1);
+
+        assertEquals(expectedEpsilonNFA, compiler.compile(rootNode));
+    }
+
+    @Test
+    public void testCompilerReturnsEpsilonNFAForConcatenationNode()
+    {
+        RegularExpressionSyntaxCompiler compiler = new RegularExpressionSyntaxCompiler();
+        ConcatenationNode rootNode = new ConcatenationNode();
+        rootNode.addChild(new CharacterNode('a'));
+        rootNode.addChild(new CharacterNode('b'));
+        rootNode.addChild(new CharacterNode('c'));
+
+        EpsilonNFAState state1 = new EpsilonNFAState("0", false);
+        EpsilonNFAState state2 = new EpsilonNFAState("0", false);
+        EpsilonNFAState state3 = new EpsilonNFAState("0", false);
+        EpsilonNFAState state4 = new EpsilonNFAState("0", false);
+        state1.addTransition(new StateTransition('a', state2));
+        state2.addTransition(new StateTransition('b', state3));
+        state3.addTransition(new StateTransition('c', state4));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state1);
 
         assertEquals(expectedEpsilonNFA, compiler.compile(rootNode));
