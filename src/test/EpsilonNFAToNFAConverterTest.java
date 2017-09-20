f454905d9a4a93d936ec74b4890f6311e0c02e47
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-19 21:50:03 -0500

    Additional placeholder unit tests for epsilon NFA to NFA conversion

--- src/test/EpsilonNFAToNFAConverterTest.java
+++ src/test/EpsilonNFAToNFAConverterTest.java
@@ -10,13 +10,25 @@ public class EpsilonNFAToNFAConverterTest
     }
 
     @Test
-    public void testSingleEpsilonTransitionEliminated()
+    public void testStartStateSingleEpsilonTransitionEliminated()
     {
         assertEquals(0, 1);
     }
 
     @Test
-    public void testFinalStatusObtainedFromStateAfterEpsilonTransition()
+    public void testNonStartStateSingleEpsilonTransitionEliminated()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testStartStateFinalStatusObtainedFromStateAfterEpsilonTransition()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testNonStartStateFinalStatusObtainedFromStateAfterEpsilonTransition()
     {
         assertEquals(0, 1);
     }
@@ -29,6 +41,12 @@ public class EpsilonNFAToNFAConverterTest
 
     @Test
     public void testMultipleEpsilonTransitionsNavigated()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCycleNavigated()
     {
         assertEquals(0, 1);
     }
