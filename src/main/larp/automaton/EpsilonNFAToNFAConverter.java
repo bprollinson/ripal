9a562a392930eb29b3711c4e4ef3f6463b156379
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-20 23:28:01 -0500

    Changing epsilon NFA to NFA converter to using recursion to satisfy another unit test

--- src/main/larp/automaton/EpsilonNFAToNFAConverter.java
+++ src/main/larp/automaton/EpsilonNFAToNFAConverter.java
@@ -6,33 +6,27 @@ public class EpsilonNFAToNFAConverter
 {
     public NFA convert(EpsilonNFA nfa)
     {
-        State startState = null;
+        return new NFA(this.convertNode(nfa.getStartState()));
+    }
 
-        Vector<State> pendingEpsilonNFAStates = new Vector<State>();
-        pendingEpsilonNFAStates.add(nfa.getStartState());
+    private State convertNode(State epsilonNFAState)
+    {
+        State startState = new NFAState("", epsilonNFAState.isAccepting());
 
-        while (pendingEpsilonNFAStates.size() > 0)
+        Vector<StateTransition> transitions = epsilonNFAState.getTransitions();
+        for (int i = 0; i < transitions.size(); i++)
         {
-            State epsilonNFAState = pendingEpsilonNFAStates.get(0);
-            startState = new NFAState("", epsilonNFAState.isAccepting());
-
-            Vector<StateTransition> transitions = epsilonNFAState.getTransitions();
-            for (int i = 0; i < transitions.size(); i++)
+            StateTransition transition = transitions.get(i);
+            if (transition.getInput() != null)
             {
-                StateTransition transition = transitions.get(i);
-                if (transition.getInput() != null)
-                {
-                    startState.addTransition(new StateTransition(transition.getInput(), new NFAState("", transition.getNextState().isAccepting())));
-                }
-                else if (transition.getNextState().isAccepting())
-                {
-                    startState.setAccepting(true);
-                }
+                startState.addTransition(new StateTransition(transition.getInput(), this.convertNode(transition.getNextState())));
             }
-
-            pendingEpsilonNFAStates.remove(0);
+            else if (transition.getNextState().isAccepting())
+            {
+                startState.setAccepting(true);
+            }
         }
 
-        return new NFA(startState);
+        return startState;
     }
 }
