772f32c70ec903cdd93b5e60567b704f967e1585
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-20 22:40:53 -0500

    Starting conversion of epsilon NFA to NFA converter handling into breadth-first search

--- src/main/larp/automaton/EpsilonNFAToNFAConverter.java
+++ src/main/larp/automaton/EpsilonNFAToNFAConverter.java
@@ -6,20 +6,31 @@ public class EpsilonNFAToNFAConverter
 {
     public NFA convert(EpsilonNFA nfa)
     {
-        State startState = new NFAState("", nfa.getStartState().isAccepting());
+        State startState = null;
 
-        Vector<StateTransition> transitions = nfa.getStartState().getTransitions();
-        for (int i = 0; i < transitions.size(); i++)
+        Vector<State> pendingEpsilonNFAStates = new Vector<State>();
+        pendingEpsilonNFAStates.add(nfa.getStartState());
+
+        while (pendingEpsilonNFAStates.size() > 0)
         {
-            StateTransition transition = transitions.get(i);
-            if (transition.getInput() != null)
+            State epsilonNFAState = pendingEpsilonNFAStates.get(0);
+            startState = new NFAState("", epsilonNFAState.isAccepting());
+
+            Vector<StateTransition> transitions = epsilonNFAState.getTransitions();
+            for (int i = 0; i < transitions.size(); i++)
             {
-                startState.addTransition(new StateTransition(transition.getInput(), new NFAState("", transition.getNextState().isAccepting())));
+                StateTransition transition = transitions.get(i);
+                if (transition.getInput() != null)
+                {
+                    startState.addTransition(new StateTransition(transition.getInput(), new NFAState("", transition.getNextState().isAccepting())));
+                }
+                else if (transition.getNextState().isAccepting())
+                {
+                    startState.setAccepting(true);
+                }
             }
-            else if (transition.getNextState().isAccepting())
-            {
-                startState.setAccepting(true);
-            }
+
+            pendingEpsilonNFAStates.remove(0);
         }
 
         return new NFA(startState);
