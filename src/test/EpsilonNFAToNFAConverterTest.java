a0f3f01f4ed3c2fd55e7c7663277d33a7b2af4e5
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-21 22:16:02 -0500

    Implemented unit test for epsilon transition final state calculation in a loop

--- src/test/EpsilonNFAToNFAConverterTest.java
+++ src/test/EpsilonNFAToNFAConverterTest.java
@@ -174,7 +174,20 @@ public class EpsilonNFAToNFAConverterTest
     @Test
     public void testCycleNavigatedForFinalStateCalculationFromEpsilonTransitions()
     {
-        assertEquals(0, 1);
+        EpsilonNFAToNFAConverter converter = new EpsilonNFAToNFAConverter();
+
+        NFAState expectedState1 = new NFAState("S0", false);
+        NFAState expectedState2 = new NFAState("S1", false);
+        expectedState1.addTransition(new StateTransition('a', expectedState2));
+        NFA expectedNFA = new NFA(expectedState1);
+
+        EpsilonNFAState state1 = new EpsilonNFAState("S0", false);
+        EpsilonNFAState state2 = new EpsilonNFAState("S1", false);
+        state1.addTransition(new StateTransition('a', state2));
+        state2.addTransition(new StateTransition(null, state2));
+        EpsilonNFA epsilonNFA = new EpsilonNFA(state1);
+
+        assertEquals(expectedNFA, converter.convert(epsilonNFA));
     }
 
     @Test
