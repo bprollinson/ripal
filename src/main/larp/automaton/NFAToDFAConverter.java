9b4193af3bd7f810395a5999ba3e63d358757596
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-23 14:31:16 -0500

    Setting up NFA to DFA converter structure to be more similar to proper setup

--- src/main/larp/automaton/NFAToDFAConverter.java
+++ src/main/larp/automaton/NFAToDFAConverter.java
@@ -1,9 +1,28 @@
 package larp.automaton;
 
+import java.util.Vector;
+
 public class NFAToDFAConverter
 {
     public DFA convert(NFA nfa)
     {
-        return new DFA(nfa.getStartState());
+        return new DFA(this.convertNode(nfa.getStartState()));
+    }
+
+    private State convertNode(State NFAState)
+    {
+        State startState = new NFAState("", false);
+
+        Vector<StateTransition> transitions = NFAState.getTransitions();
+        for (int i = 0; i < transitions.size(); i++)
+        {
+            StateTransition transition = transitions.get(i);
+            if (transition.getInput() != null)
+            {
+                startState.addTransition(new StateTransition(transition.getInput(), transition.getNextState()));
+            }
+        }
+
+        return startState;
     }
 }
