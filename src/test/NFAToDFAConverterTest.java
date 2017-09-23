5a0794695316aa643640d44820475892fe6b02ca
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-09-23 13:03:08 -0500

    Adding test for state combination in NFA to DFA conversion

--- src/test/NFAToDFAConverterTest.java
+++ src/test/NFAToDFAConverterTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.automaton.DFA;
+import larp.automaton.DFAState;
 import larp.automaton.NFA;
 import larp.automaton.NFAState;
 import larp.automaton.NFAToDFAConverter;
@@ -19,5 +21,25 @@ public class NFAToDFAConverterTest
         NFA expectedNFA = new NFA(expectedState1);
 
         assertEquals(expectedNFA, converter.convert(expectedNFA));
+    }
+
+    @Test
+    public void testMultipleTransitionsForSameCharacterCombinedFromStartState()
+    {
+        NFAToDFAConverter converter = new NFAToDFAConverter();
+
+        DFAState expectedState1 = new DFAState("S0", false);
+        DFAState expectedState2 = new DFAState("S0", false);
+        expectedState1.addTransition(new StateTransition('a', expectedState2));
+        DFA expectedDFA = new DFA(expectedState1);
+
+        NFAState state1 = new NFAState("S0", false);
+        NFAState state2 = new NFAState("S1", false);
+        NFAState state3 = new NFAState("S2", false);
+        state1.addTransition(new StateTransition('a', state2));
+        state2.addTransition(new StateTransition('a', state3));
+        NFA NFA = new NFA(state1);
+
+        assertEquals(expectedDFA, converter.convert(NFA));
     }
 }
