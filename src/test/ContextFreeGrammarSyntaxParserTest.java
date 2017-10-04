8a4988fa05bdbec0cd342582fafbaafdcd73360a
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-04 06:09:41 -0500

    Adding unit tests for CFG syntax parser

---
+++ src/test/ContextFreeGrammarSyntaxParserTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ContextFreeGrammarSyntaxParserTest
+{
+    @Test
+    public void testParseReturnsTreeForEmptyRightHandSize()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testParseReturnsTreeForSingleTerminalRightHandSize()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testParseReturnsTreeForSingleNonTerminalRightHandSize()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testParseReturnsTreeForMultipleTokenRightHandSize()
+    {
+        assertEquals(0, 1);
+    }
+}
