d5732657f8614586cd723dc1b8eba32a640b7eeb
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-09 22:09:51 -0500

    Filling in some implementation details of production node test

--- src/test/ProductionNodeTest.java
+++ src/test/ProductionNodeTest.java
@@ -1,29 +1,53 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
+import larp.grammar.contextfreelanguage.ConcatenationNode;
+import larp.grammar.contextfreelanguage.ProductionNode;
 
 public class ProductionNodeTest
 {
     @Test
     public void testEqualsReturnsTrueForProductionNodeWithNoChildren()
     {
-        assertEquals(0, 1);
+        ProductionNode node = new ProductionNode();
+
+        assertTrue(node.equals(new ProductionNode()));
     }
 
     @Test
     public void testEqualsReturnsTrueForProductionNodeWithSameSubtree()
     {
-        assertEquals(0, 1);
+        ProductionNode node = new ProductionNode();
+        node.addChild(new ProductionNode());
+        node.addChild(new ProductionNode());
+
+        ProductionNode expectedNode = new ProductionNode();
+        expectedNode.addChild(new ProductionNode());
+        expectedNode.addChild(new ProductionNode());
+
+        assertTrue(node.equals(expectedNode));
     }
 
     @Test
     public void testEqualsReturnsFalseForProductionNodeWithDifferentSubtree()
     {
-        assertEquals(0, 1);
+        ProductionNode node = new ProductionNode();
+        node.addChild(new ProductionNode());
+        node.addChild(new ProductionNode());
+
+        ProductionNode expectedNode = new ProductionNode();
+        expectedNode.addChild(new ProductionNode());
+        expectedNode.addChild(new ConcatenationNode());
+
+        assertFalse(node.equals(expectedNode));
     }
 
     @Test
     public void testEqualsReturnsFalseForNodeOfOtherType()
     {
-        assertEquals(0, 1);
+        ProductionNode node = new ProductionNode();
+
+        assertFalse(node.equals(new ConcatenationNode()));
     }
 }
