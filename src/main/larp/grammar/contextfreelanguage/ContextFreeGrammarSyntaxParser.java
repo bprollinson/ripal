51896966391351afc4b024cc25f6a57558bbdb40
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-11 06:44:27 -0500

    Got all remaining unit tests passing

--- src/main/larp/grammar/contextfreelanguage/ContextFreeGrammarSyntaxParser.java
+++ src/main/larp/grammar/contextfreelanguage/ContextFreeGrammarSyntaxParser.java
@@ -10,7 +10,24 @@ public class ContextFreeGrammarSyntaxParser
         NonTerminalToken nonTerminalToken = (NonTerminalToken)tokens.get(0);
         NonTerminalNode nonTerminalNode = new NonTerminalNode(nonTerminalToken.getName());
         productionNode.addChild(nonTerminalNode);
-        productionNode.addChild(new ConcatenationNode());
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        productionNode.addChild(concatenationNode);
+
+        for (int i = 2; i < tokens.size(); i++)
+        {
+            ContextFreeGrammarSyntaxToken token = tokens.get(i);
+
+            if (token instanceof NonTerminalToken)
+            {
+                NonTerminalToken innerToken = (NonTerminalToken)token;
+                concatenationNode.addChild(new NonTerminalNode(innerToken.getName()));
+            }
+            else if (token instanceof TerminalToken)
+            {
+                TerminalToken innerToken = (TerminalToken)token;
+                concatenationNode.addChild(new TerminalNode(innerToken.getValue()));
+            }
+        }
 
         return productionNode;
     }
