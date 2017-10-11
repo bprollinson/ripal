04fdafdab38002e28ef9e648469bd70d8ac2090e
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-10 19:12:51 -0500

    Filling some implementation details of terminal node test

--- src/test/TerminalNodeTest.java
+++ src/test/TerminalNodeTest.java
@@ -1,23 +1,33 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
+import larp.grammar.contextfreelanguage.ConcatenationNode;
+import larp.grammar.contextfreelanguage.TerminalNode;
 
 public class TerminalNodeTest
 {
     @Test
     public void testEqualsReturnsTrueForTerminalNodeWithSameValue()
     {
-        assertEquals(0, 1);
+        TerminalNode node = new TerminalNode("a");
+
+        assertTrue(node.equals(new TerminalNode("a")));
     }
 
     @Test
     public void testEqualsReturnsFalseForTerminalNodeWithDifferentValue()
     {
-        assertEquals(0, 1);
+        TerminalNode node = new TerminalNode("a");
+
+        assertFalse(node.equals(new TerminalNode("b")));
     }
 
     @Test
     public void testEqualsReturnsFalseForNodeOfOtherType()
     {
-        assertEquals(0, 1);
+        TerminalNode node = new TerminalNode("a");
+
+        assertFalse(node.equals(new ConcatenationNode()));
     }
 }
