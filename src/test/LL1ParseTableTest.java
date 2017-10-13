fe3864608c37d3849e93f01a0b84d112b071b440
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-12 21:25:59 -0500

    Making LL1 parse table tests more descriptive

--- src/test/LL1ParseTableTest.java
+++ src/test/LL1ParseTableTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 public class LL1ParseTableTest
 {
     @Test
-    public void testAcceptsReturnsTrueForSingleCharacterCFG()
+    public void testAcceptsReturnsTrueForCorrectCharacterInSingleCharacterCFG()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testAcceptsReturnsTrueForIncorrectCharacterInSingleCharacterCFG()
     {
         assertEquals(0, 1);
     }
