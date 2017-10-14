a8b6f74811a3a73ec9b1e03e32192f82db056ca5
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-13 19:08:45 -0500

    Placeholder test for getting start symbol from CFG

---
+++ src/test/ContextFreeGrammarTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ContextFreeGrammarTest
+{
+    @Test
+    public void testGetStartSymbolReturnsStartSymbol()
+    {
+        assertEquals(0, 1);
+    }
+}
