e0fbd433b0a3cad2a978c20986f5530d6f718fd0
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-14 17:11:39 -0500

    Proper start symbol CFG unit test

--- src/test/ContextFreeGrammarTest.java
+++ src/test/ContextFreeGrammarTest.java
@@ -1,11 +1,26 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.grammar.contextfreelanguage.NonTerminalNode;
+import larp.grammar.contextfreelanguage.ProductionNode;
+import larp.grammar.contextfreelanguage.TerminalNode;
+import larp.parsetable.ContextFreeGrammar;
+
 public class ContextFreeGrammarTest
 {
     @Test
     public void testGetStartSymbolReturnsStartSymbol()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammar contextFreeGrammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        productionNode.addChild(new TerminalNode("a"));
+        contextFreeGrammar.addProduction(productionNode);
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("T"));
+        productionNode.addChild(new TerminalNode("b"));
+        contextFreeGrammar.addProduction(productionNode);
+
+        assertEquals(new NonTerminalNode("S"), contextFreeGrammar.getStartSymbol());
     }
 }
