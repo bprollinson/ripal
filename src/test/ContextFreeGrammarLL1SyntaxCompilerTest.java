d9673223069f58949add5e5edf86cc304efda330
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-19 07:43:59 -0400

    Unit test for CFG production LL1 parse table construction with one terminal and one nonterminal production

--- src/test/ContextFreeGrammarLL1SyntaxCompilerTest.java
+++ src/test/ContextFreeGrammarLL1SyntaxCompilerTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import larp.automaton.contextfreelanguage.ContextFreeGrammarLL1SyntaxCompiler;
+import larp.grammar.contextfreelanguage.ConcatenationNode;
 import larp.grammar.contextfreelanguage.NonTerminalNode;
 import larp.grammar.contextfreelanguage.ProductionNode;
 import larp.grammar.contextfreelanguage.TerminalNode;
@@ -18,10 +19,40 @@ public class ContextFreeGrammarLL1SyntaxCompilerTest
         ContextFreeGrammar grammar = new ContextFreeGrammar();
         ProductionNode productionNode = new ProductionNode();
         productionNode.addChild(new NonTerminalNode("S"));
-        productionNode.addChild(new TerminalNode("a"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
         grammar.addProduction(productionNode);
+
         LL1ParseTable expectedTable = new LL1ParseTable(grammar);
         expectedTable.addCell(new NonTerminalNode("S"), new TerminalNode("a"), 0);
+
+        assertEquals(expectedTable, compiler.compile(grammar));
+    }
+
+    @Test
+    public void testCompileReturnsParseTableForSingleNonTerminalAndSingleTerminalProductionCFG()
+    {
+        ContextFreeGrammarLL1SyntaxCompiler compiler = new ContextFreeGrammarLL1SyntaxCompiler();
+
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("A"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        LL1ParseTable expectedTable = new LL1ParseTable(grammar);
+        expectedTable.addCell(new NonTerminalNode("S"), new TerminalNode("a"), 0);
+        expectedTable.addCell(new NonTerminalNode("A"), new TerminalNode("a"), 0);
 
         assertEquals(expectedTable, compiler.compile(grammar));
     }
