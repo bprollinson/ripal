361001d3c7ebec2db05181555f48552bae3987d9
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-20 19:00:59 -0400

    Filling in implementation of another LL1 syntax compiler test

--- src/test/ContextFreeGrammarLL1SyntaxCompilerTest.java
+++ src/test/ContextFreeGrammarLL1SyntaxCompilerTest.java
@@ -161,10 +161,41 @@ public class ContextFreeGrammarLL1SyntaxCompilerTest
         compiler.compile(grammar);
     }
 
-    @Test
+    @Test(expected = AmbiguousLL1ParseTableException.class)
     public void testCompileThrowsExceptionForFirstAmbiguityBetweenTwoNonTerminals()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammarLL1SyntaxCompiler compiler = new ContextFreeGrammarLL1SyntaxCompiler();
+
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("A"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("B"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("A"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("B"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        compiler.compile(grammar);
     }
 
     @Test
