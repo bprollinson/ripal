088c7dcfbb4d3087a7ee3a8bb986a54236e78b7b
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-24 07:40:29 -0400

    Restructuring LL1 syntax compiler

--- src/main/larp/automaton/contextfreelanguage/ContextFreeGrammarLL1SyntaxCompiler.java
+++ src/main/larp/automaton/contextfreelanguage/ContextFreeGrammarLL1SyntaxCompiler.java
@@ -6,6 +6,9 @@ import larp.grammar.contextfreelanguage.TerminalNode;
 import larp.parsetable.ContextFreeGrammar;
 import larp.parsetable.LL1ParseTable;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Vector;
 
 public class ContextFreeGrammarLL1SyntaxCompiler
@@ -16,23 +19,36 @@ public class ContextFreeGrammarLL1SyntaxCompiler
 
         Vector<ContextFreeGrammarSyntaxNode> productions = grammar.getProductions();
 
+        HashMap<NonTerminalNode, HashSet<Integer>> nonTerminalRules = new HashMap<NonTerminalNode, HashSet<Integer>>();
+
         for (int i = 0; i < productions.size(); i++)
         {
             ContextFreeGrammarSyntaxNode productionNode = productions.get(i);
-            TerminalNode first = this.getFirst(productionNode);
+            HashSet<Integer> existingSet = nonTerminalRules.get(productionNode.getChildNodes().get(0));
+            if (existingSet == null)
+            {
+                existingSet = new HashSet<Integer>();
+            }
+            existingSet.add(i);
+            nonTerminalRules.put((NonTerminalNode)productionNode.getChildNodes().get(0), existingSet);
+        }
+
+        for (Map.Entry<NonTerminalNode, HashSet<Integer>> entry : nonTerminalRules.entrySet()) {
+            Integer firstRuleIndex = entry.getValue().iterator().next();
+            TerminalNode first = this.getFirst(grammar, firstRuleIndex);
 
             if (first != null)
             {
-                parseTable.addCell((NonTerminalNode)productionNode.getChildNodes().get(0), first, i);
+                parseTable.addCell(entry.getKey(), first, firstRuleIndex);
             }
         }
 
         return parseTable;
     }
 
-    private TerminalNode getFirst(ContextFreeGrammarSyntaxNode productionNode)
+    private TerminalNode getFirst(ContextFreeGrammar grammar, int ruleIndex)
     {
-        ContextFreeGrammarSyntaxNode concatenationNode = productionNode.getChildNodes().get(1);
+        ContextFreeGrammarSyntaxNode concatenationNode = grammar.getProduction(ruleIndex).getChildNodes().get(1);
         if (concatenationNode.getChildNodes().get(0) instanceof TerminalNode)
         {
             return (TerminalNode)concatenationNode.getChildNodes().get(0);
