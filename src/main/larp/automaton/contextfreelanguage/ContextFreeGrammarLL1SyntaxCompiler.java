6f807f7e4b527a47994c3197dbc9ac768d2ace58
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-24 00:06:28 -0400

    Getting first LL1 table construction test to pass

--- src/main/larp/automaton/contextfreelanguage/ContextFreeGrammarLL1SyntaxCompiler.java
+++ src/main/larp/automaton/contextfreelanguage/ContextFreeGrammarLL1SyntaxCompiler.java
@@ -1,12 +1,43 @@
 package larp.automaton.contextfreelanguage;
 
+import larp.grammar.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.grammar.contextfreelanguage.NonTerminalNode;
+import larp.grammar.contextfreelanguage.TerminalNode;
 import larp.parsetable.ContextFreeGrammar;
 import larp.parsetable.LL1ParseTable;
+
+import java.util.Vector;
 
 public class ContextFreeGrammarLL1SyntaxCompiler
 {
     public LL1ParseTable compile(ContextFreeGrammar grammar)
     {
+        LL1ParseTable parseTable = new LL1ParseTable(grammar);
+
+        Vector<ContextFreeGrammarSyntaxNode> productions = grammar.getProductions();
+
+        for (int i = 0; i < productions.size(); i++)
+        {
+            ContextFreeGrammarSyntaxNode productionNode = productions.get(i);
+            TerminalNode first = this.getFirst(productionNode);
+
+            if (first != null)
+            {
+                parseTable.addCell((NonTerminalNode)productionNode.getChildNodes().get(0), first, i);
+            }
+        }
+
+        return parseTable;
+    }
+
+    private TerminalNode getFirst(ContextFreeGrammarSyntaxNode productionNode)
+    {
+        ContextFreeGrammarSyntaxNode concatenationNode = productionNode.getChildNodes().get(1);
+        if (concatenationNode.getChildNodes().get(0) instanceof TerminalNode)
+        {
+            return (TerminalNode)concatenationNode.getChildNodes().get(0);
+        }
+
         return null;
     }
 }
