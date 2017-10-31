69baea29f40c888ec479515c60ad7f3e36807e76
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-31 07:08:26 -0400

    Unit test for getting first terminal from after epsilon

--- src/test/FirstSetCalculatorTest.java
+++ src/test/FirstSetCalculatorTest.java
@@ -127,4 +127,29 @@ public class FirstSetCalculatorTest
         expectedFirsts.add(new EpsilonNode());
         assertEquals(expectedFirsts, calculator.getFirst(0));
     }
+
+    @Test
+    public void testGetFirstReturnsSubsequentTerminalFromAfterEpsilonNonTerminal()
+    {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("A"));
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("A"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new EpsilonNode());
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        FirstSetCalculator calculator = new FirstSetCalculator(grammar);
+        HashSet<TerminalNode> expectedFirsts = new HashSet<TerminalNode>();
+        expectedFirsts.add(new TerminalNode("b"));
+        assertEquals(expectedFirsts, calculator.getFirst(0));
+    }
 }
