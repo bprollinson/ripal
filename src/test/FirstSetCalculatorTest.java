7baa9cc77f5069e9f38eefd326135654d4f9c1a5
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-31 07:43:37 -0400

    Unit test for terminal in first after series of epslion nonterminals

--- src/test/FirstSetCalculatorTest.java
+++ src/test/FirstSetCalculatorTest.java
@@ -217,4 +217,37 @@ public class FirstSetCalculatorTest
         expectedFirsts.add(new EpsilonNode());
         assertEquals(expectedFirsts, calculator.getFirst(0));
     }
+
+    @Test
+    public void testGetFirstReturnsTerminalAfterSeriesOfEpsilonNonTerminals()
+    {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("A"));
+        concatenationNode.addChild(new NonTerminalNode("B"));
+        concatenationNode.addChild(new TerminalNode("c"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("A"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new EpsilonNode());
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("B"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new EpsilonNode());
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        FirstSetCalculator calculator = new FirstSetCalculator(grammar);
+        HashSet<ContextFreeGrammarSyntaxNode> expectedFirsts = new HashSet<ContextFreeGrammarSyntaxNode>();
+        expectedFirsts.add(new TerminalNode("c"));
+        assertEquals(expectedFirsts, calculator.getFirst(0));
+    }
 }
