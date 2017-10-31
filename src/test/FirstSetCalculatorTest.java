ea78f874f6a4b3b46d5cb20a16975fad32e7e756
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-30 23:32:58 -0400

    Unit test for multi-value first value calculation

--- src/test/FirstSetCalculatorTest.java
+++ src/test/FirstSetCalculatorTest.java
@@ -52,4 +52,36 @@ public class FirstSetCalculatorTest
         expectedFirsts.add(new TerminalNode("a"));
         assertEquals(expectedFirsts, calculator.getFirst(0));
     }
+
+    @Test
+    public void testGetFirstReturnsMultipleTerminalsWithIntermediateNonTerminalProductions()
+    {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("A"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("A"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("A"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        FirstSetCalculator calculator = new FirstSetCalculator(grammar);
+        HashSet<TerminalNode> expectedFirsts = new HashSet<TerminalNode>();
+        expectedFirsts.add(new TerminalNode("a"));
+        expectedFirsts.add(new TerminalNode("b"));
+        assertEquals(expectedFirsts, calculator.getFirst(0));
+    }
 }
