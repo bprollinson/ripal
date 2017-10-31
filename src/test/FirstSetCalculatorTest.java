ba13450eccebea28ed017af6f110aaebedd353b0
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-30 23:35:54 -0400

    Unit test for cycle detection during first set calculation

--- src/test/FirstSetCalculatorTest.java
+++ src/test/FirstSetCalculatorTest.java
@@ -84,4 +84,28 @@ public class FirstSetCalculatorTest
         expectedFirsts.add(new TerminalNode("b"));
         assertEquals(expectedFirsts, calculator.getFirst(0));
     }
+
+    @Test
+    public void testGetFirstDetectsCycle()
+    {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("S"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        FirstSetCalculator calculator = new FirstSetCalculator(grammar);
+        HashSet<TerminalNode> expectedFirsts = new HashSet<TerminalNode>();
+        expectedFirsts.add(new TerminalNode("a"));
+        assertEquals(expectedFirsts, calculator.getFirst(0));
+    }
 }
