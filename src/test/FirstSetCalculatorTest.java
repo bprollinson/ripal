424c19421d2ca9aedf3c03a69ea0a53bf1584fdb
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-31 07:20:27 -0400

    Unit test for multiple nonterminals going to epsilon in first calculation

--- src/test/FirstSetCalculatorTest.java
+++ src/test/FirstSetCalculatorTest.java
@@ -154,7 +154,7 @@ public class FirstSetCalculatorTest
     }
 
     @Test
-    public void testGetFirstReturnsMultipleTerminalsWhenFirstNonTerminalGoesToTerminalOrNull()
+    public void testGetFirstReturnsMultipleTerminalsWhenFirstNonTerminalGoesToTerminalOrEpsilon()
     {
         ContextFreeGrammar grammar = new ContextFreeGrammar();
         ProductionNode productionNode = new ProductionNode();
@@ -183,6 +183,38 @@ public class FirstSetCalculatorTest
         HashSet<TerminalNode> expectedFirsts = new HashSet<TerminalNode>();
         expectedFirsts.add(new TerminalNode("a"));
         expectedFirsts.add(new TerminalNode("b"));
+        assertEquals(expectedFirsts, calculator.getFirst(0));
+    }
+
+    @Test
+    public void testGetFirstReturnsEpsilonWhenAllNonTerminalsGoToEpsilon()
+    {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("A"));
+        concatenationNode.addChild(new NonTerminalNode("B"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("A"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new EpsilonNode());
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("B"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new EpsilonNode());
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        FirstSetCalculator calculator = new FirstSetCalculator(grammar);
+        HashSet<ContextFreeGrammarSyntaxNode> expectedFirsts = new HashSet<ContextFreeGrammarSyntaxNode>();
+        expectedFirsts.add(new EpsilonNode());
         assertEquals(expectedFirsts, calculator.getFirst(0));
     }
 }
