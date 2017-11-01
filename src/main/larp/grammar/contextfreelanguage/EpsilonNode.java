b759ac3f756c1fb974993c16d42eee3f71d0a3f4
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-10-31 20:59:00 -0400

    Getting two epsilon first set calculation unit tests passing

--- src/main/larp/grammar/contextfreelanguage/EpsilonNode.java
+++ src/main/larp/grammar/contextfreelanguage/EpsilonNode.java
@@ -2,4 +2,8 @@ package larp.grammar.contextfreelanguage;
 
 public class EpsilonNode extends ContextFreeGrammarSyntaxNode
 {
+    public int hashCode()
+    {
+        return "".hashCode();
+    }
 }
