1c51d3a4bd8c9e7a7b469d42e74f01c0aa5c3486
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-11-03 07:48:28 -0400

    Implementation for unit test testing follow set for start symbol followed by terminal

--- src/test/FollowSetCalculatorTest.java
+++ src/test/FollowSetCalculatorTest.java
@@ -30,4 +30,30 @@ public class FollowSetCalculatorTest
         expectedFollows.add(new EndOfStringNode());
         assertEquals(expectedFollows, calculator.getFollow(new NonTerminalNode("S")));
     }
+
+    @Test
+    public void testGetFollowReturnsTerminalNodeAndEndOfStringSymbolForStartTerminalFollowedByTerminal()
+    {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new NonTerminalNode("S"));
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        FollowSetCalculator calculator = new FollowSetCalculator(grammar);
+        HashSet<ContextFreeGrammarSyntaxNode> expectedFollows = new HashSet<ContextFreeGrammarSyntaxNode>();
+        expectedFollows.add(new EndOfStringNode());
+        expectedFollows.add(new TerminalNode("b"));
+        assertEquals(expectedFollows, calculator.getFollow(new NonTerminalNode("S")));
+    }
 }
