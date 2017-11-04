2f350025bc14ea670ca648472b92a257207dad94
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-11-04 14:47:03 -0400

    Getting a bunch of follow set calculation unit tests passing

--- src/main/larp/automaton/contextfreelanguage/FollowSetCalculator.java
+++ src/main/larp/automaton/contextfreelanguage/FollowSetCalculator.java
@@ -1,12 +1,15 @@
 package larp.automaton.contextfreelanguage;
 
+import larp.grammar.contextfreelanguage.ConcatenationNode;
 import larp.grammar.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 import larp.grammar.contextfreelanguage.EndOfStringNode;
 import larp.grammar.contextfreelanguage.NonTerminalNode;
+import larp.grammar.contextfreelanguage.TerminalNode;
 import larp.parsetable.ContextFreeGrammar;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Vector;
 
 public class FollowSetCalculator
 {
@@ -19,6 +22,24 @@ public class FollowSetCalculator
         this.follows = new HashMap<NonTerminalNode, HashSet<ContextFreeGrammarSyntaxNode>>();
 
         this.add(grammar.getStartSymbol(), new EndOfStringNode());
+        Vector<ContextFreeGrammarSyntaxNode> productions = grammar.getProductions();
+        for (int i = 0; i < productions.size(); i++)
+        {
+            ContextFreeGrammarSyntaxNode production = productions.get(i);
+            ContextFreeGrammarSyntaxNode rightHandSide = production.getChildNodes().get(1);
+            ContextFreeGrammarSyntaxNode previousNode = null;
+            for (int j = 0; j < rightHandSide.getChildNodes().size(); j++)
+            {
+                ContextFreeGrammarSyntaxNode node = rightHandSide.getChildNodes().get(j);
+
+                if (previousNode instanceof NonTerminalNode && node instanceof TerminalNode)
+                {
+                    this.add((NonTerminalNode)previousNode, node);
+                }
+
+                previousNode = node;
+            }
+        }
     }
 
     public HashSet<ContextFreeGrammarSyntaxNode> getFollow(NonTerminalNode nonTerminal)
