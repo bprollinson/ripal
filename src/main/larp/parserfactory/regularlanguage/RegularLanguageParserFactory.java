38ecd736f0aef80afd7987ac3dc454239d758e6c
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-12-21 07:42:18 -0500

    Unit test implementation and some placeholder dependencies for regular language parser factory

--- src/main/larp/parserfactory/regularlanguage/RegularLanguageParserFactory.java
+++ src/main/larp/parserfactory/regularlanguage/RegularLanguageParserFactory.java
@@ -1,5 +1,25 @@
 package larp.parserfactory.regularlanguage;
 
+import larp.parser.regularlanguage.DFA;
+import larp.syntaxcompiler.regularlanguage.RegularExpressionSyntaxCompiler;
+import larp.syntaxparser.regularlanguage.RegularExpressionSyntaxParser;
+import larp.syntaxtokenizer.regularlanguage.RegularExpressionSyntaxTokenizer;
+
 public class RegularLanguageParserFactory
 {
+    private RegularExpressionSyntaxTokenizer tokenizer;
+    private RegularExpressionSyntaxParser parser;
+    private RegularExpressionSyntaxCompiler compiler;
+
+    public RegularLanguageParserFactory()
+    {
+        this.tokenizer = new RegularExpressionSyntaxTokenizer();
+        this.parser = new RegularExpressionSyntaxParser();
+        this.compiler = new RegularExpressionSyntaxCompiler();
+    }
+
+    public DFA factory(String regularExpression)
+    {
+        return null;
+    }
 }
--- src/test/RegularLanguageParserFactoryTest.java
+++ src/test/RegularLanguageParserFactoryTest.java
@@ -1,12 +1,24 @@
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
+import larp.parser.regularlanguage.DFA;
+import larp.parser.regularlanguage.DFAState;
+import larp.parser.regularlanguage.StateTransition;
 import larp.parserfactory.regularlanguage.RegularLanguageParserFactory;
 
 public class RegularLanguageParserFactoryTest
 {
     @Test
-    public void testConstruct()
+    public void testFactoryCreatesDFAForRegularExpression()
     {
         RegularLanguageParserFactory factory = new RegularLanguageParserFactory();
+        DFA dfa = factory.factory("a");
+
+        DFAState state1 = new DFAState("0", false);
+        DFAState state2 = new DFAState("0", true);
+        state1.addTransition(new StateTransition('a', state2));
+        DFA expectedDFA = new DFA(state1);
+
+        assertTrue(expectedDFA.structureEquals(dfa));
     }
 }
