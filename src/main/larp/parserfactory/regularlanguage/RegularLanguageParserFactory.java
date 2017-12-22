aac6add1cdaf4efa33253b4aec7eb21b3fc7a1d3
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-12-21 23:35:14 -0500

    Getting regular language parser factory test passing

--- src/main/larp/parserfactory/regularlanguage/RegularLanguageParserFactory.java
+++ src/main/larp/parserfactory/regularlanguage/RegularLanguageParserFactory.java
@@ -1,6 +1,11 @@
 package larp.parserfactory.regularlanguage;
 
 import larp.parser.regularlanguage.DFA;
+import larp.parser.regularlanguage.EpsilonNFA;
+import larp.parser.regularlanguage.EpsilonNFAToNFAConverter;
+import larp.parser.regularlanguage.NFA;
+import larp.parser.regularlanguage.NFAToDFAConverter;
+import larp.parsetree.regularlanguage.RegularExpressionSyntaxNode;
 import larp.syntaxcompiler.regularlanguage.RegularExpressionSyntaxCompiler;
 import larp.syntaxparser.regularlanguage.RegularExpressionSyntaxParser;
 import larp.syntaxtokenizer.regularlanguage.RegularExpressionSyntaxTokenizer;
@@ -14,18 +19,25 @@ public class RegularLanguageParserFactory
     private RegularExpressionSyntaxTokenizer tokenizer;
     private RegularExpressionSyntaxParser parser;
     private RegularExpressionSyntaxCompiler compiler;
+    private EpsilonNFAToNFAConverter epsilonNFAToNFAConverter;
+    private NFAToDFAConverter NFAToDFAConverter;
 
     public RegularLanguageParserFactory()
     {
         this.tokenizer = new RegularExpressionSyntaxTokenizer();
         this.parser = new RegularExpressionSyntaxParser();
         this.compiler = new RegularExpressionSyntaxCompiler();
+        this.epsilonNFAToNFAConverter = new EpsilonNFAToNFAConverter();
+        this.NFAToDFAConverter = new NFAToDFAConverter();
     }
 
     public DFA factory(String regularExpression) throws RegularExpressionSyntaxTokenizerException
     {
         List<RegularExpressionSyntaxToken> tokenList = this.tokenizer.tokenize(regularExpression);
+        RegularExpressionSyntaxNode rootNode = this.parser.parse(tokenList);
+        EpsilonNFA enfa = this.compiler.compile(rootNode);
+        NFA nfa = this.epsilonNFAToNFAConverter.convert(enfa);
 
-        return null;
+        return this.NFAToDFAConverter.convert(nfa);
     }
 }
