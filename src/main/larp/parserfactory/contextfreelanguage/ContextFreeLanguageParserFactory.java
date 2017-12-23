11d8f5ccd0ae46928a13470757fdbf11cc39cfa7
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-12-23 00:03:24 -0500

    Filling in placeholder unit test for context free language parser factory

---
+++ src/main/larp/parserfactory/contextfreelanguage/ContextFreeLanguageParserFactory.java
@@ -0,0 +1,13 @@
+package larp.parserfactory.contextfreelanguage;
+
+import larp.parser.contextfreelanguage.LL1Parser;
+
+import java.util.List;
+
+public class ContextFreeLanguageParserFactory
+{
+    public LL1Parser factory(List<String> input)
+    {
+        return null;
+    }
+}
--- src/test/ContextFreeLanguageParserFactoryTest.java
+++ src/test/ContextFreeLanguageParserFactoryTest.java
@@ -1,11 +1,38 @@
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parser.contextfreelanguage.AmbiguousLL1ParseTableException;
+import larp.parser.contextfreelanguage.LL1Parser;
+import larp.parser.contextfreelanguage.LL1ParseTable;
+import larp.parserfactory.contextfreelanguage.ContextFreeLanguageParserFactory;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContextFreeLanguageParserFactoryTest
 {
     @Test
-    public void testFactoryCreatesDFAForRegularExpression()
+    public void testFactoryCreatesDFAForRegularExpression() throws AmbiguousLL1ParseTableException
     {
-        assertTrue(false);
+        ContextFreeLanguageParserFactory factory = new ContextFreeLanguageParserFactory();
+        List<String> input = new ArrayList<String>();
+        input.add("S: AB");
+        input.add("A: \"a\"");
+        input.add("B: \"b\"");
+        LL1Parser parser = factory.factory(input);
+
+        ContextFreeGrammar expectedCFG = new ContextFreeGrammar();
+        expectedCFG.addProduction(new NonTerminalNode("S"), new NonTerminalNode("A"), new NonTerminalNode("B"));
+        expectedCFG.addProduction(new NonTerminalNode("A"), new TerminalNode("a"));
+        expectedCFG.addProduction(new NonTerminalNode("B"), new TerminalNode("b"));
+        LL1ParseTable expectedTable = new LL1ParseTable(expectedCFG);
+        expectedTable.addCell(new NonTerminalNode("S"), new TerminalNode("a"), 0);
+        expectedTable.addCell(new NonTerminalNode("A"), new TerminalNode("a"), 0);
+        expectedTable.addCell(new NonTerminalNode("B"), new TerminalNode("b"), 1);
+        LL1Parser expectedParser = new LL1Parser(expectedTable);
+        assertEquals(expectedParser, parser);
     }
 }
