f6dc338ff56c6b9180f20ee49160d2085f1216ec
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-12-23 09:10:28 -0500

    Adding parse table construction step to context free language parser factory

--- src/main/larp/parserfactory/contextfreelanguage/ContextFreeLanguageParserFactory.java
+++ src/main/larp/parserfactory/contextfreelanguage/ContextFreeLanguageParserFactory.java
@@ -1,8 +1,11 @@
 package larp.parserfactory.contextfreelanguage;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parser.contextfreelanguage.AmbiguousLL1ParseTableException;
 import larp.parser.contextfreelanguage.LL1Parser;
+import larp.parser.contextfreelanguage.LL1ParseTable;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.syntaxcompiler.contextfreelanguage.ContextFreeGrammarLL1SyntaxCompiler;
 import larp.syntaxparser.contextfreelanguage.ContextFreeGrammarSyntaxParser;
 import larp.syntaxtokenizer.contextfreelanguage.ContextFreeGrammarSyntaxTokenizer;
 import larp.syntaxtokenizer.contextfreelanguage.ContextFreeGrammarSyntaxTokenizerException;
@@ -14,14 +17,16 @@ public class ContextFreeLanguageParserFactory
 {
     private ContextFreeGrammarSyntaxTokenizer tokenizer;
     private ContextFreeGrammarSyntaxParser parser;
+    private ContextFreeGrammarLL1SyntaxCompiler compiler;
 
     public ContextFreeLanguageParserFactory()
     {
         this.tokenizer = new ContextFreeGrammarSyntaxTokenizer();
         this.parser = new ContextFreeGrammarSyntaxParser();
+        this.compiler = new ContextFreeGrammarLL1SyntaxCompiler();
     }
 
-    public LL1Parser factory(List<String> input) throws ContextFreeGrammarSyntaxTokenizerException
+    public LL1Parser factory(List<String> input) throws ContextFreeGrammarSyntaxTokenizerException, AmbiguousLL1ParseTableException
     {
         ContextFreeGrammar cfg = new ContextFreeGrammar();
         for (String inputString: input)
@@ -30,6 +35,8 @@ public class ContextFreeLanguageParserFactory
             ContextFreeGrammarSyntaxNode rootNode = this.parser.parse(tokenList);
             cfg.addProduction(rootNode);
         }
+
+        LL1ParseTable parseTable = this.compiler.compile(cfg);
 
         return null;
     }
