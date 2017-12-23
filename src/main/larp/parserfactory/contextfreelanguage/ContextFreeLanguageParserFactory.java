7d34b423960b7df9e8c550a22c67bbca6debb207
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-12-23 08:49:59 -0500

    Filling in paring step in context free language parser factory

--- src/main/larp/parserfactory/contextfreelanguage/ContextFreeLanguageParserFactory.java
+++ src/main/larp/parserfactory/contextfreelanguage/ContextFreeLanguageParserFactory.java
@@ -1,6 +1,8 @@
 package larp.parserfactory.contextfreelanguage;
 
 import larp.parser.contextfreelanguage.LL1Parser;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.syntaxparser.contextfreelanguage.ContextFreeGrammarSyntaxParser;
 import larp.syntaxtokenizer.contextfreelanguage.ContextFreeGrammarSyntaxTokenizer;
 import larp.syntaxtokenizer.contextfreelanguage.ContextFreeGrammarSyntaxTokenizerException;
 import larp.token.contextfreelanguage.ContextFreeGrammarSyntaxToken;
@@ -10,10 +12,12 @@ import java.util.List;
 public class ContextFreeLanguageParserFactory
 {
     private ContextFreeGrammarSyntaxTokenizer tokenizer;
+    private ContextFreeGrammarSyntaxParser parser;
 
     public ContextFreeLanguageParserFactory()
     {
         this.tokenizer = new ContextFreeGrammarSyntaxTokenizer();
+        this.parser = new ContextFreeGrammarSyntaxParser();
     }
 
     public LL1Parser factory(List<String> input) throws ContextFreeGrammarSyntaxTokenizerException
@@ -21,6 +25,7 @@ public class ContextFreeLanguageParserFactory
         for (String inputString: input)
         {
             List<ContextFreeGrammarSyntaxToken> tokenList = this.tokenizer.tokenize(inputString);
+            ContextFreeGrammarSyntaxNode rootNode = this.parser.parse(tokenList);
         }
 
         return null;
