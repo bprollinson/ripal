9a5738713b021c0ba3e91bef8e7044d7c25e7c0f
Brendan Rollinson <brendan.rollinson@gmail.com>
2017-12-29 13:26:53 -0500

    Adding unit test for tokenizing space before character in regular expression

--- src/test/RegularExpressionSyntaxTokenizerTest.java
+++ src/test/RegularExpressionSyntaxTokenizerTest.java
@@ -59,7 +59,7 @@ public class RegularExpressionSyntaxTokenizerTest
     }
 
     @Test
-    public void testTokenizerTokenizesExpressionContainingSpaceAfterCharacter() throws RegularExpressionSyntaxTokenizerException
+    public void testTokenizerTokenizesExpressionContainingCharacterBeforeSpace() throws RegularExpressionSyntaxTokenizerException
     {
         RegularExpressionSyntaxTokenizer tokenizer = new RegularExpressionSyntaxTokenizer();
 
@@ -67,6 +67,19 @@ public class RegularExpressionSyntaxTokenizerTest
         List<RegularExpressionSyntaxToken> expectedResult = new ArrayList<RegularExpressionSyntaxToken>();
         expectedResult.add(new CharacterToken('a'));
         expectedResult.add(new CharacterToken(' '));
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testTokenizerTokenizesExpressionContainingCharacterAfterSpace() throws RegularExpressionSyntaxTokenizerException
+    {
+        RegularExpressionSyntaxTokenizer tokenizer = new RegularExpressionSyntaxTokenizer();
+
+        List<RegularExpressionSyntaxToken> result = tokenizer.tokenize(" a");
+        List<RegularExpressionSyntaxToken> expectedResult = new ArrayList<RegularExpressionSyntaxToken>();
+        expectedResult.add(new CharacterToken(' '));
+        expectedResult.add(new CharacterToken('a'));
 
         assertEquals(expectedResult, result);
     }
