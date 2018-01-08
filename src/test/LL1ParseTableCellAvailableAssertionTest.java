e2a874dd9712dacd0d94d9c9943d1c38d6674878
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-01-08 07:07:35 -0500

    Unit test implementation for LL1 parse table cell available assertion tests

--- src/test/LL1ParseTableCellAvailableAssertionTest.java
+++ src/test/LL1ParseTableCellAvailableAssertionTest.java
@@ -1,16 +1,33 @@
 import org.junit.Test;
 
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parser.contextfreelanguage.AmbiguousLL1ParseTableException;
+import larp.parser.contextfreelanguage.LL1ParseTable;
+import larp.parser.contextfreelanguage.LL1ParseTableCellAvailableAssertion;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
 
 public class LL1ParseTableCellAvailableAssertionTest
 {
     @Test(expected = AmbiguousLL1ParseTableException.class)
     public void testValidateThrowsExceptionForCellThatAlreadyExists() throws AmbiguousLL1ParseTableException
     {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        LL1ParseTable parseTable = new LL1ParseTable(cfg);
+        parseTable.addCell(new NonTerminalNode("S"), new TerminalNode("a"), 0);
+        LL1ParseTableCellAvailableAssertion assertion = new LL1ParseTableCellAvailableAssertion(parseTable, new NonTerminalNode("S"), new TerminalNode("a"));
+
+        assertion.validate();
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
     public void testValidateDoesNotThrowExceptionForCellThatDoesNotAlreadyExist() throws AmbiguousLL1ParseTableException
     {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        LL1ParseTable parseTable = new LL1ParseTable(cfg);
+        parseTable.addCell(new NonTerminalNode("S"), new TerminalNode("a"), 0);
+        LL1ParseTableCellAvailableAssertion assertion = new LL1ParseTableCellAvailableAssertion(parseTable, new NonTerminalNode("S"), new TerminalNode("b"));
+
+        assertion.validate();
     }
 }
