eae0b3bf915ed680ec484742d3e71dbcfa76dec1
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-19 20:36:26 -0400

    Placeholder unit test for grammar augmentor

---
+++ src/test/ContextFreeGrammarAugmentorTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ContextFreeGrammarAugmentorTest
+{
+    @Test
+    public void testAugmentAddsNewStartStateToValidGrammar()
+    {
+        assertEquals(0, 1);
+    }
+}
