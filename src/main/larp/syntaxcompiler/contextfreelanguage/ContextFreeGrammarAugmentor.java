6f6cd3576273e72284043364dbf7f86b8d605b3b
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-20 07:13:05 -0400

    First hack at augment method implementation

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarAugmentor.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarAugmentor.java
@@ -1,11 +1,24 @@
 package larp.syntaxcompiler.contextfreelanguage;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+
+import java.util.List;
 
 public class ContextFreeGrammarAugmentor
 {
     public ContextFreeGrammar augment(ContextFreeGrammar cfg)
     {
-        return cfg;
+        ContextFreeGrammar newCfg = new ContextFreeGrammar();
+        newCfg.addProduction(new NonTerminalNode("S'"), cfg.getStartSymbol());
+
+        List<ContextFreeGrammarSyntaxNode> productions = cfg.getProductions();
+        for (ContextFreeGrammarSyntaxNode production: productions)
+        {
+            newCfg.addProduction(production);
+        }
+
+        return newCfg;
     }
 }
