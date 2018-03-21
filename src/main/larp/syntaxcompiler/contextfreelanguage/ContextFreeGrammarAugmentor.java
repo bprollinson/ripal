3afb4dcfc2ac45544ad58c0dc082b3571c404c28
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-20 23:10:45 -0400

    Getting unit test related to resolving naming conflict in grammar augmentor passing

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarAugmentor.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarAugmentor.java
@@ -4,7 +4,9 @@ import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 import larp.parsetree.contextfreelanguage.NonTerminalNode;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ContextFreeGrammarAugmentor
 {
@@ -13,8 +15,7 @@ public class ContextFreeGrammarAugmentor
         ContextFreeGrammar newCfg = new ContextFreeGrammar();
         newCfg.addProduction(this.calculateNewStartSymbol(cfg), cfg.getStartSymbol());
 
-        List<ContextFreeGrammarSyntaxNode> productions = cfg.getProductions();
-        for (ContextFreeGrammarSyntaxNode production: productions)
+        for (ContextFreeGrammarSyntaxNode production: cfg.getProductions())
         {
             newCfg.addProduction(production);
         }
@@ -24,6 +25,28 @@ public class ContextFreeGrammarAugmentor
 
     private NonTerminalNode calculateNewStartSymbol(ContextFreeGrammar cfg)
     {
-        return new NonTerminalNode(cfg.getStartSymbol().getName() + "'");
+        Set<String> existingNames = new HashSet<String>();
+        for (ContextFreeGrammarSyntaxNode production: cfg.getProductions())
+        {
+            NonTerminalNode leftHandSide = (NonTerminalNode)production.getChildNodes().get(0);
+            existingNames.add(leftHandSide.getName());
+
+            ContextFreeGrammarSyntaxNode rightHandSide = production.getChildNodes().get(1);
+            for (ContextFreeGrammarSyntaxNode childNode: rightHandSide.getChildNodes())
+            {
+                if (childNode instanceof NonTerminalNode)
+                {
+                    existingNames.add(((NonTerminalNode)childNode).getName());
+                }
+            }
+        }
+
+        String newStartSymbolName = cfg.getStartSymbol().getName() + "'";
+        while (existingNames.contains(newStartSymbolName))
+        {
+            newStartSymbolName += "'";
+        }
+
+        return new NonTerminalNode(newStartSymbolName);
     }
 }
