a68f469acfa4abacfbdbbe916e6a2655478edfa4
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-21 07:43:34 -0400

    Getting cfg augmentor unit tests passing

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarAugmentor.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarAugmentor.java
@@ -2,6 +2,7 @@ package larp.syntaxcompiler.contextfreelanguage;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.EndOfStringNode;
 import larp.parsetree.contextfreelanguage.NonTerminalNode;
 
 import java.util.HashSet;
@@ -13,7 +14,7 @@ public class ContextFreeGrammarAugmentor
     public ContextFreeGrammar augment(ContextFreeGrammar cfg)
     {
         ContextFreeGrammar newCfg = new ContextFreeGrammar();
-        newCfg.addProduction(this.calculateNewStartSymbol(cfg), cfg.getStartSymbol());
+        newCfg.addProduction(this.calculateNewStartSymbol(cfg), cfg.getStartSymbol(), new EndOfStringNode());
 
         for (ContextFreeGrammarSyntaxNode production: cfg.getProductions())
         {
