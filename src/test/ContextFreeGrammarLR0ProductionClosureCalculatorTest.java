218056f2e8a4f654d7cb0b97152d0d10296ab0b7
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-22 07:15:26 -0400

    Placeholder unit tests for calculate an LR grammar production closure

--- src/test/ContextFreeGrammarLR0ProductionClosureCalculatorTest.java
+++ src/test/ContextFreeGrammarLR0ProductionClosureCalculatorTest.java
@@ -1,5 +1,47 @@
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ContextFreeGrammarLR0ProductionClosureCalculatorTest
 {
+    @Test
+    public void testCalculateClosureDoesNotAddItemsWhenDotNotPresent()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateClosureDoesNotAddItemsWhenDotIsNotBeforeNonTerminal()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateClosureDoesNotAddItemsWhenDotAtEndOfProduction()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateClosureAddsSingleProductionWhenDotIsBeforeNonTerminalWithSingleProduction()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateClosureAddsMultipleProductionsWhenDotIsBeforeNonTerminalWithMultipleProductions()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateClosureRemovesDuplicateProduction()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateClosureAddsSingleProductionWhenSetContainsOneRelevantAndOneIrrelevantProduction()
+    {
+        assertEquals(0, 1);
+    }
 }
