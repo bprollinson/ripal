2aa15a971507854a96db149de50a2ff871ed9227
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-21 20:31:15 -0400

    Adding class for dot node to be used later in LR 0 parser setup

---
+++ src/main/larp/parsetree/contextfreelanguage/DotNode.java
@@ -0,0 +1,9 @@
+package larp.parsetree.contextfreelanguage;
+
+public class DotNode extends ContextFreeGrammarSyntaxNode
+{
+    public int hashCode()
+    {
+        return ".".hashCode();
+    }
+}
