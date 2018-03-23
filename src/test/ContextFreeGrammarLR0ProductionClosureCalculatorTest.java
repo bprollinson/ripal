fc46e9a1b0cf2077d58f5a9bbf595684a857b2eb
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-22 22:09:30 -0400

    Adding another unit test for calculating an LR grammar production closure

--- src/test/ContextFreeGrammarLR0ProductionClosureCalculatorTest.java
+++ src/test/ContextFreeGrammarLR0ProductionClosureCalculatorTest.java
@@ -72,7 +72,20 @@ public class ContextFreeGrammarLR0ProductionClosureCalculatorTest
     @Test
     public void testCalculateClosureAddsSingleProductionWhenDotIsBeforeNonTerminalWithSingleProduction()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammarLR0ProductionClosureCalculator calculator = new ContextFreeGrammarLR0ProductionClosureCalculator();
+
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new NonTerminalNode("A"));
+        cfg.addProduction(new NonTerminalNode("A"), new NonTerminalNode("B"));
+
+        Set<ContextFreeGrammarSyntaxNode> productionSet = new HashSet<ContextFreeGrammarSyntaxNode>();
+        productionSet.add(this.buildProduction(new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+
+        Set<ContextFreeGrammarSyntaxNode> expectedProductionSet = new HashSet<ContextFreeGrammarSyntaxNode>();
+        expectedProductionSet.add(this.buildProduction(new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        expectedProductionSet.add(this.buildProduction(new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("B")));
+
+        assertEquals(expectedProductionSet, calculator.calculateClosure(cfg, productionSet));
     }
 
     @Test
