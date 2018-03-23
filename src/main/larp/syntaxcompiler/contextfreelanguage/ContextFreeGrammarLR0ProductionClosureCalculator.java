6c3cc44857d0715db33535c828d433f0fcf69113
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-22 21:42:21 -0400

    Implementation of first unit test for calculating an LR grammar production closure

---
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0ProductionClosureCalculator.java
@@ -0,0 +1,13 @@
+package larp.syntaxcompiler.contextfreelanguage;
+
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+
+import java.util.Set;
+
+public class ContextFreeGrammarLR0ProductionClosureCalculator
+{
+    public Set<ContextFreeGrammarSyntaxNode> calculateClosure(Set<ContextFreeGrammarSyntaxNode> productions)
+    {
+        return null;
+    }
+}
--- src/test/ContextFreeGrammarLR0ProductionClosureCalculatorTest.java
+++ src/test/ContextFreeGrammarLR0ProductionClosureCalculatorTest.java
@@ -1,12 +1,30 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.parsetree.contextfreelanguage.ConcatenationNode;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.ProductionNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+import larp.syntaxcompiler.contextfreelanguage.ContextFreeGrammarLR0ProductionClosureCalculator;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContextFreeGrammarLR0ProductionClosureCalculatorTest
 {
     @Test
     public void testCalculateClosureDoesNotAddItemsWhenDotNotPresent()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammarLR0ProductionClosureCalculator calculator = new ContextFreeGrammarLR0ProductionClosureCalculator();
+
+        Set<ContextFreeGrammarSyntaxNode> productionSet = new HashSet<ContextFreeGrammarSyntaxNode>();
+        productionSet.add(this.buildProduction(new NonTerminalNode("S"), new TerminalNode("a")));
+
+        Set<ContextFreeGrammarSyntaxNode> expectedProductionSet = new HashSet<ContextFreeGrammarSyntaxNode>();
+        expectedProductionSet.add(this.buildProduction(new NonTerminalNode("S"), new TerminalNode("a")));
+
+        assertEquals(expectedProductionSet, calculator.calculateClosure(productionSet));
     }
 
     @Test
@@ -43,5 +61,20 @@ public class ContextFreeGrammarLR0ProductionClosureCalculatorTest
     public void testCalculateClosureAddsSingleProductionWhenSetContainsOneRelevantAndOneIrrelevantProduction()
     {
         assertEquals(0, 1);
+    }
+
+    private ProductionNode buildProduction(NonTerminalNode nonTerminalNode, ContextFreeGrammarSyntaxNode... rightHandNodes)
+    {
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(nonTerminalNode);
+
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        for (ContextFreeGrammarSyntaxNode rightHandNode: rightHandNodes)
+        {
+            concatenationNode.addChild(rightHandNode);
+        }
+        productionNode.addChild(concatenationNode);
+
+        return productionNode;
     }
 }
