02d9e619c138032b1266bef27195859d77cb46f3
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-23 23:40:32 -0400

    First hack at baseline cfg production closure calculation

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0ProductionClosureCalculator.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0ProductionClosureCalculator.java
@@ -3,12 +3,16 @@ package larp.syntaxcompiler.contextfreelanguage;
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class ContextFreeGrammarLR0ProductionClosureCalculator
 {
     public Set<ContextFreeGrammarSyntaxNode> calculateClosure(ContextFreeGrammar cfg, Set<ContextFreeGrammarSyntaxNode> productions)
     {
-        return null;
+        Set<ContextFreeGrammarSyntaxNode> productionsClosure = new HashSet<ContextFreeGrammarSyntaxNode>();
+        productionsClosure.addAll(productions);
+
+        return productionsClosure;
     }
 }
