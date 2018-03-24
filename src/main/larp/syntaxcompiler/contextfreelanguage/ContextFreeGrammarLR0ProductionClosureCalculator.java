d2c4e72fce21e683fc8268e3b0d03240d42bfa43
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-24 18:40:12 -0400

    Working through grammar closure calculation

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0ProductionClosureCalculator.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0ProductionClosureCalculator.java
@@ -2,9 +2,12 @@ package larp.syntaxcompiler.contextfreelanguage;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.DotNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
 import larp.util.SetMap;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ContextFreeGrammarLR0ProductionClosureCalculator
@@ -14,12 +17,13 @@ public class ContextFreeGrammarLR0ProductionClosureCalculator
         Set<ContextFreeGrammarSyntaxNode> productionsClosure = new HashSet<ContextFreeGrammarSyntaxNode>();
         productionsClosure.addAll(productions);
 
-        SetMap<ContextFreeGrammarSyntaxNode, Integer> startingTerminalProductions = this.calculateStartingTerminalProductionsMap(cfg);
+        SetMap<ContextFreeGrammarSyntaxNode, Integer> startingNonTerminalProductions = this.calculateStartingNonTerminalProductionsMap(cfg);
+        this.expandClosure(cfg, startingNonTerminalProductions, productionsClosure);
 
         return productionsClosure;
     }
 
-    private SetMap<ContextFreeGrammarSyntaxNode, Integer> calculateStartingTerminalProductionsMap(ContextFreeGrammar cfg)
+    private SetMap<ContextFreeGrammarSyntaxNode, Integer> calculateStartingNonTerminalProductionsMap(ContextFreeGrammar cfg)
     {
         SetMap<ContextFreeGrammarSyntaxNode, Integer> startingTerminalProductions = new SetMap<ContextFreeGrammarSyntaxNode, Integer>();
         for (int i = 0; i < cfg.getProductions().size(); i++)
@@ -28,5 +32,35 @@ public class ContextFreeGrammarLR0ProductionClosureCalculator
         }
 
         return startingTerminalProductions;
+    }
+
+    private void expandClosure(ContextFreeGrammar cfg, SetMap<ContextFreeGrammarSyntaxNode, Integer> startingNonTerminalProductions, Set<ContextFreeGrammarSyntaxNode> productionsClosure)
+    {
+        for (ContextFreeGrammarSyntaxNode production: productionsClosure)
+        {
+            List<ContextFreeGrammarSyntaxNode> childNodes = production.getChildNodes();
+            boolean lastNodeWasDot = false;
+
+            for (int i = 0; i < childNodes.get(1).getChildNodes().size(); i++)
+            {
+                ContextFreeGrammarSyntaxNode childNode = childNodes.get(1).getChildNodes().get(i);
+                if (childNode instanceof DotNode)
+                {
+                    lastNodeWasDot = true;
+                    continue;
+                }
+
+                if (childNode instanceof NonTerminalNode && lastNodeWasDot)
+                {
+                    this.addProductionsForNonTerminal(cfg, startingNonTerminalProductions.get(childNode), productionsClosure);
+                }
+
+                lastNodeWasDot = false;
+            }
+        }
+    }
+
+    private void addProductionsForNonTerminal(ContextFreeGrammar cfg, Set<Integer> productions, Set<ContextFreeGrammarSyntaxNode> productionsClosure)
+    {
     }
 }
