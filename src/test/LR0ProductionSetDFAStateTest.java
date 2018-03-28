153d075b011ee48ea975d3a0c3f8527391b1bed0
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-03-28 07:12:12 -0400

    Placeholder LR0 production state DFA state unit tests

--- src/test/LR0ProductionSetDFAStateTest.java
+++ src/test/LR0ProductionSetDFAStateTest.java
@@ -1,3 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class LR0ProductionSetDFAStateTest
 {
+    @Test
+    public void testStructureEqualsReturnsTrueForSameProductionSet()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testStructureEqualsReturnsTrueForSameSubsequentProductionSet()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testStructureEqualsReturnsFalseForDifferentProductionSet()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testStructureEqualsReturnsFalseForDifferentSubsequentProductionSet()
+    {
+        assertEquals(0, 1);
+    }
 }
