239212039d77fafaaa5f29f1cefcaf40b8354e18
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-05 07:10:15 -0400

    Adding first placeholder unit test for LR0 production set DFA compiler

--- src/test/ContextFreeGrammarLR0ProductionSetDFACompilerTest.java
+++ src/test/ContextFreeGrammarLR0ProductionSetDFACompilerTest.java
@@ -1,5 +1,11 @@
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ContextFreeGrammarLR0ProductionSetDFACompilerTest
 {
+    @Test
+    public void testCompileCreatesDFAForTerminalRule()
+    {
+        assertEquals(0, 1);
+    }
 }
