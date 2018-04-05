f901a625bfba8e506dff3a984c57e24372fd96c4
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-05 07:14:50 -0400

    Adding another placeholder unit test for LR0 production set DFA compiler

--- src/test/ContextFreeGrammarLR0ProductionSetDFACompilerTest.java
+++ src/test/ContextFreeGrammarLR0ProductionSetDFACompilerTest.java
@@ -8,4 +8,10 @@ public class ContextFreeGrammarLR0ProductionSetDFACompilerTest
     {
         assertEquals(0, 1);
     }
+
+    @Test
+    public void testCompileCreatesDFAForNonTerminalRule()
+    {
+        assertEquals(0, 1);
+    }
 }
