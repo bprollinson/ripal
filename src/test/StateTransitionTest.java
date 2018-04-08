d164faff3912ce901600a63fcf47d35d796043cb
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-08 18:13:52 -0400

    Adding placeholder unit tests for state transitions

--- src/test/StateTransitionTest.java
+++ src/test/StateTransitionTest.java
@@ -1,3 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class StateTransitionTest
 {
+    @Test
+    public void testInputEqualsReturnsTrueWhenInputMatchesExpectedInput()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testInputEqualsReturnsFalseWhenInputDoesNotMatchExpectedInput()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testInputEqualsReturnsTrueWhenEmptyInputMatchesExpectedEmptyInput()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testInputEqualsReturnsFalseWhenEmptyInputDoesNotMatchExpectedEmptyInput()
+    {
+        assertEquals(0, 1);
+    }
 }
