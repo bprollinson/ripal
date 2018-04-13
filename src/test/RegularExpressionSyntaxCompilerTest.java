62de3d3a9694a7d1435016a5536d1cd3a7ce13a3
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-12 22:03:58 -0400

    Cleaning up warnings related to not specifying proper input type for state transitions in NFA to DFA converter unit test

--- src/test/RegularExpressionSyntaxCompilerTest.java
+++ src/test/RegularExpressionSyntaxCompilerTest.java
@@ -19,7 +19,7 @@ public class RegularExpressionSyntaxCompilerTest
         CharacterNode rootNode = new CharacterNode('a');
 
         EpsilonNFAState state = new EpsilonNFAState("S0", false);
-        state.addTransition(new StateTransition('a', new EpsilonNFAState("S1", true)));
+        state.addTransition(new StateTransition<Character>('a', new EpsilonNFAState("S1", true)));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state);
 
         assertTrue(expectedEpsilonNFA.structureEquals(compiler.compile(rootNode)));
@@ -34,9 +34,9 @@ public class RegularExpressionSyntaxCompilerTest
 
         EpsilonNFAState state1 = new EpsilonNFAState("S0", false);
         EpsilonNFAState state2 = new EpsilonNFAState("S1", true);
-        state1.addTransition(new StateTransition('a', state2));
-        state1.addTransition(new StateTransition(null, state2));
-        state2.addTransition(new StateTransition(null, state1));
+        state1.addTransition(new StateTransition<Character>('a', state2));
+        state1.addTransition(new StateTransition<Character>(null, state2));
+        state2.addTransition(new StateTransition<Character>(null, state1));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state1);
 
         assertTrue(expectedEpsilonNFA.structureEquals(compiler.compile(rootNode)));
@@ -50,7 +50,7 @@ public class RegularExpressionSyntaxCompilerTest
 
         EpsilonNFAState state1 = new EpsilonNFAState("0", false);
         EpsilonNFAState state2 = new EpsilonNFAState("0", true);
-        state1.addTransition(new StateTransition(null, state2));
+        state1.addTransition(new StateTransition<Character>(null, state2));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state1);
 
         assertTrue(expectedEpsilonNFA.structureEquals(compiler.compile(rootNode)));
@@ -66,7 +66,7 @@ public class RegularExpressionSyntaxCompilerTest
         EpsilonNFAState state1 = new EpsilonNFAState("0", false);
         EpsilonNFAState state2 = new EpsilonNFAState("0", true);
 
-        state1.addTransition(new StateTransition('a', state2));
+        state1.addTransition(new StateTransition<Character>('a', state2));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state1);
 
         assertTrue(expectedEpsilonNFA.structureEquals(compiler.compile(rootNode)));
@@ -87,11 +87,11 @@ public class RegularExpressionSyntaxCompilerTest
         EpsilonNFAState state4 = new EpsilonNFAState("0", false);
         EpsilonNFAState state5 = new EpsilonNFAState("0", false);
         EpsilonNFAState state6 = new EpsilonNFAState("0", true);
-        state1.addTransition(new StateTransition('a', state2));
-        state2.addTransition(new StateTransition(null, state3));
-        state3.addTransition(new StateTransition('b', state4));
-        state4.addTransition(new StateTransition(null, state5));
-        state5.addTransition(new StateTransition('c', state6));
+        state1.addTransition(new StateTransition<Character>('a', state2));
+        state2.addTransition(new StateTransition<Character>(null, state3));
+        state3.addTransition(new StateTransition<Character>('b', state4));
+        state4.addTransition(new StateTransition<Character>(null, state5));
+        state5.addTransition(new StateTransition<Character>('c', state6));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(state1);
 
         assertTrue(expectedEpsilonNFA.structureEquals(compiler.compile(rootNode)));
@@ -114,15 +114,15 @@ public class RegularExpressionSyntaxCompilerTest
         EpsilonNFAState lowerState1 = new EpsilonNFAState("0", false);
         EpsilonNFAState lowerState2 = new EpsilonNFAState("0", false);
         EpsilonNFAState endState = new EpsilonNFAState("0", true);
-        startState.addTransition(new StateTransition(null, upperState1));
-        startState.addTransition(new StateTransition(null, middleState1));
-        startState.addTransition(new StateTransition(null, lowerState1));
-        upperState1.addTransition(new StateTransition('a', upperState2));
-        middleState1.addTransition(new StateTransition('b', middleState2));
-        lowerState1.addTransition(new StateTransition('c', lowerState2));
-        upperState2.addTransition(new StateTransition(null, endState));
-        middleState2.addTransition(new StateTransition(null, endState));
-        lowerState2.addTransition(new StateTransition(null, endState));
+        startState.addTransition(new StateTransition<Character>(null, upperState1));
+        startState.addTransition(new StateTransition<Character>(null, middleState1));
+        startState.addTransition(new StateTransition<Character>(null, lowerState1));
+        upperState1.addTransition(new StateTransition<Character>('a', upperState2));
+        middleState1.addTransition(new StateTransition<Character>('b', middleState2));
+        lowerState1.addTransition(new StateTransition<Character>('c', lowerState2));
+        upperState2.addTransition(new StateTransition<Character>(null, endState));
+        middleState2.addTransition(new StateTransition<Character>(null, endState));
+        lowerState2.addTransition(new StateTransition<Character>(null, endState));
         EpsilonNFA expectedEpsilonNFA = new EpsilonNFA(startState);
 
         assertTrue(expectedEpsilonNFA.structureEquals(compiler.compile(rootNode)));
