41e917b0f838d645bdb3af650e5be3e49c93ac30
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-14 18:49:09 -0400

    Getting DFA unit tests for checking state type passing

--- src/main/larp/parser/regularlanguage/DFA.java
+++ src/main/larp/parser/regularlanguage/DFA.java
@@ -10,12 +10,22 @@ public class DFA extends FiniteAutomata
     public boolean accepts(String inputString)
     {
         State currentState = this.startState;
+        if (!(currentState instanceof DFAState))
+        {
+            return false;
+        }
+
         int characterPosition = 0;
 
         while (currentState != null && characterPosition < inputString.length())
         {
             Character inputCharacter = inputString.charAt(characterPosition);
             currentState = ((DFAState)currentState).getNextState(inputCharacter);
+
+            if (!(currentState instanceof DFAState))
+            {
+                return false;
+            }
 
             characterPosition++;
         }
