4efa7be1ea2e8bfac05d37e3a01edd543d9ff83c
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-15 16:29:22 -0400

    Adding placeholder unit tests for DFA class

--- src/test/DFAStateTest.java
+++ src/test/DFAStateTest.java
@@ -1,3 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class DFAStateTest
 {
+    @Test
+    public void testGetNextStateReturnsNullWhenInputDoesNotMatchAnyTransitionInput()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetNextStateReturnsNextStateFromSingleMatchingTransition()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetNextSTateReturnsNextStateFromSubsequentMatchingTransition()
+    {
+        assertEquals(0, 1);
+    }
 }
