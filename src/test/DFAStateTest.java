fcfb563f6d25260a53916f63c1391e2a0d0ab3be
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-15 16:37:20 -0400

    Implementing unit tests for DFAState class

--- src/test/DFAStateTest.java
+++ src/test/DFAStateTest.java
@@ -1,23 +1,38 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.parser.regularlanguage.DFAState;
+import larp.parser.regularlanguage.StateTransition;
+
 public class DFAStateTest
 {
     @Test
     public void testGetNextStateReturnsNullWhenInputDoesNotMatchAnyTransitionInput()
     {
-        assertEquals(0, 1);
+        DFAState state = new DFAState("S0", true);
+
+        assertEquals(null, state.getNextState('a'));
     }
 
     @Test
     public void testGetNextStateReturnsNextStateFromSingleMatchingTransition()
     {
-        assertEquals(0, 1);
+        DFAState state0 = new DFAState("S0", true);
+        DFAState state1 = new DFAState("S1", true);
+        state0.addTransition(new StateTransition<Character>('a', state1));
+
+        assertEquals(state1, state0.getNextState('a'));
     }
 
     @Test
     public void testGetNextSTateReturnsNextStateFromSubsequentMatchingTransition()
     {
-        assertEquals(0, 1);
+        DFAState state0 = new DFAState("S0", true);
+        DFAState state1 = new DFAState("S1", true);
+        DFAState state2 = new DFAState("S2", true);
+        state0.addTransition(new StateTransition<Character>('a', state1));
+        state0.addTransition(new StateTransition<Character>('b', state2));
+
+        assertEquals(state2, state0.getNextState('b'));
     }
 }
