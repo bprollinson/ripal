f950d00a38e0e15a22fab9fa004eacd8bf78a0a5
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-04-21 11:41:20 -0400

    Extracting a comparator class from the state class

--- src/main/larp/parser/regularlanguage/State.java
+++ src/main/larp/parser/regularlanguage/State.java
@@ -59,97 +59,112 @@ public abstract class State<I, S extends State> implements ComparableStructure
 
     protected boolean equalsState(State otherState, List<State> ourCoveredStates, List<State> otherCoveredStates)
     {
-        if (!otherState.getClass().equals(this.getClass()))
+        return new StateComparator().equalsState(this, otherState, ourCoveredStates, otherCoveredStates);
+    }
+
+    private class StateComparator
+    {
+        public boolean equalsState(State state, State otherState, List<State> ourCoveredStates, List<State> otherCoveredStates)
         {
-            return false;
-        }
-
-        if (this.accepting != otherState.isAccepting())
-        {
-            return false;
-        }
-
-        if (this.countTransitions() != otherState.countTransitions())
-        {
-            return false;
-        }
-
-        List<StateTransition> ourTransitions = new ArrayList<StateTransition>(this.transitions);
-        List<StateTransition> otherTransitions = new ArrayList<StateTransition>();
-        for (Object rawStateTransition: otherState.getTransitions())
-        {
-            StateTransition stateTransition = (StateTransition)rawStateTransition;
-            StateTransition<Object, State> testTransition = new StateTransition<Object, State>(stateTransition.getInput(), stateTransition.getNextState());
-            otherTransitions.add(testTransition);
-        }
-
-        List<State> ourNextCoveredStates = new ArrayList<State>(ourCoveredStates);
-        ourNextCoveredStates.add(this);
-        List<State> otherNextCoveredStates = new ArrayList<State>(otherCoveredStates);
-        otherNextCoveredStates.add(otherState);
-
-        while (ourTransitions.size() > 0)
-        {
-            boolean found = false;
-
-            StateTransition current = ourTransitions.get(0);
-
-            for (int i = 0; i < otherTransitions.size(); i++)
-            {
-                StateTransition otherTransition = otherTransitions.get(i);
-
-                if (current.inputEqualsOtherTransition(otherTransition))
-                {
-                    int ourNextStatePosition = ourCoveredStates.indexOf(current.getNextState());
-                    boolean ourNextStateLoops = ourNextStatePosition != -1;
-                    int otherNextStatePosition = otherCoveredStates.indexOf(otherTransition.getNextState());
-                    boolean otherNextStateLoops = otherNextStatePosition != -1;
-                    boolean nextStatesLoop = ourNextStateLoops && otherNextStateLoops;
-
-                    if (ourNextStateLoops && !nextStatesLoop || otherNextStateLoops && !nextStatesLoop)
-                    {
-                        return false;
-                    }
-
-                    if (nextStatesLoop && ourNextStatePosition != otherNextStatePosition)
-                    {
-                        return false;
-                    }
-
-                    boolean nextStatesEqual = false;
-                    if (!nextStatesLoop)
-                    {
-                        nextStatesEqual = current.getNextState().equalsState(otherTransition.getNextState(), ourNextCoveredStates, otherNextCoveredStates);
-                    }
-                    boolean equal = nextStatesEqual || nextStatesLoop;
-                    if (nextStatesEqual)
-                    {
-                        for (int j = 0; j < ourNextCoveredStates.size(); j++)
-                        {
-                            ourCoveredStates.add(ourNextCoveredStates.get(j));
-                            otherCoveredStates.add(otherNextCoveredStates.get(j));
-                        }
-                    }
-
-                    if (equal)
-                    {
-                        found = true;
-                        ourTransitions.remove(0);
-                        otherTransitions.remove(i);
-                        break;
-                    }
-                }
-            }
-
-            if (!found)
+            if (!otherState.getClass().equals(state.getClass()))
             {
                 return false;
             }
+
+            if (state.isAccepting() != otherState.isAccepting())
+            {
+                return false;
+            }
+
+            if (state.countTransitions() != otherState.countTransitions())
+            {
+                return false;
+            }
+
+            List<StateTransition> ourTransitions = new ArrayList<StateTransition>();
+            for (Object rawStateTransition: state.getTransitions())
+            {
+                StateTransition stateTransition = (StateTransition)rawStateTransition;
+                StateTransition<Object, State> testTransition = new StateTransition<Object, State>(stateTransition.getInput(), stateTransition.getNextState());
+                ourTransitions.add(testTransition);
+            }
+
+            List<StateTransition> otherTransitions = new ArrayList<StateTransition>();
+            for (Object rawStateTransition: otherState.getTransitions())
+            {
+                StateTransition stateTransition = (StateTransition)rawStateTransition;
+                StateTransition<Object, State> testTransition = new StateTransition<Object, State>(stateTransition.getInput(), stateTransition.getNextState());
+                otherTransitions.add(testTransition);
+            }
+
+            List<State> ourNextCoveredStates = new ArrayList<State>(ourCoveredStates);
+            ourNextCoveredStates.add(state);
+            List<State> otherNextCoveredStates = new ArrayList<State>(otherCoveredStates);
+            otherNextCoveredStates.add(otherState);
+
+            while (ourTransitions.size() > 0)
+            {
+                boolean found = false;
+
+                StateTransition current = ourTransitions.get(0);
+
+                for (int i = 0; i < otherTransitions.size(); i++)
+                {
+                    StateTransition otherTransition = otherTransitions.get(i);
+
+                    if (current.inputEqualsOtherTransition(otherTransition))
+                    {
+                        int ourNextStatePosition = ourCoveredStates.indexOf(current.getNextState());
+                        boolean ourNextStateLoops = ourNextStatePosition != -1;
+                        int otherNextStatePosition = otherCoveredStates.indexOf(otherTransition.getNextState());
+                        boolean otherNextStateLoops = otherNextStatePosition != -1;
+                        boolean nextStatesLoop = ourNextStateLoops && otherNextStateLoops;
+
+                        if (ourNextStateLoops && !nextStatesLoop || otherNextStateLoops && !nextStatesLoop)
+                        {
+                            return false;
+                        }
+
+                        if (nextStatesLoop && ourNextStatePosition != otherNextStatePosition)
+                        {
+                            return false;
+                        }
+
+                        boolean nextStatesEqual = false;
+                        if (!nextStatesLoop)
+                        {
+                            nextStatesEqual = current.getNextState().equalsState(otherTransition.getNextState(), ourNextCoveredStates, otherNextCoveredStates);
+                        }
+                        boolean equal = nextStatesEqual || nextStatesLoop;
+                        if (nextStatesEqual)
+                        {
+                            for (int j = 0; j < ourNextCoveredStates.size(); j++)
+                            {
+                                ourCoveredStates.add(ourNextCoveredStates.get(j));
+                                otherCoveredStates.add(otherNextCoveredStates.get(j));
+                            }
+                        }
+
+                        if (equal)
+                        {
+                            found = true;
+                            ourTransitions.remove(0);
+                            otherTransitions.remove(i);
+                            break;
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    return false;
+                }
+            }
+
+            ourCoveredStates.add(state);
+            otherCoveredStates.add(otherState);
+
+            return true;
         }
-
-        ourCoveredStates.add(this);
-        otherCoveredStates.add(otherState);
-
-        return true;
     }
 }
