1750819bd0262a0431a51a0330029f77b1c02613
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-01 07:10:29 -0400

    Adding placeholder closure calculator unit tests

--- src/test/ContextFreeGrammarClosureCalculatorTest.java
+++ src/test/ContextFreeGrammarClosureCalculatorTest.java
@@ -1,3 +1,101 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class ContextFreeGrammarClosureCalculatorTest
 {
+    @Test
+    public void testCalculateAddsSimpleTerminalProduction()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateAddsSimpleNonTerminalProduction()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateDoesNotAddProductionWhenNoneAvailable()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateDoesNotAddProductionWhenNonReachable()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateAddsTerminalChain()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateAddsClosureFromNonFirstNonTerminalInProduction()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateDoesNotAddProductionsForNonTerminal()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateDoesNotAddProductionsForSubsequentNonTerminal()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateCalculatesClosureIgnoringImmediateInfiniteLoop()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateCalculatesClosureIgnoringEventualInfiniteLoop()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateExpandsImmediateNonTerminalIntoParallelTerminals()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateExpandsEventualNonTerminalIntoParallelTerminals()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateExpandsImmediateNonTerminalIntoParallelNonTerminals()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateExpandsEventualNonTerminalIntoParallelNonTerminals()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateAddsTerminalChainInReverseOrder()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testCalculateDoesNotExpandProductionSetWhenDotMissing()
+    {
+        assertEquals(0, 1);
+    }
 }
