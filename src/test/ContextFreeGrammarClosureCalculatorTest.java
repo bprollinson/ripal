92843a904d7ae43739a787f1abee071c6230862d
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-06 14:27:04 -0400

    Implementing another cfg closure calculator unit test

--- src/test/ContextFreeGrammarClosureCalculatorTest.java
+++ src/test/ContextFreeGrammarClosureCalculatorTest.java
@@ -254,7 +254,22 @@ public class ContextFreeGrammarClosureCalculatorTest
     @Test
     public void testCalculateExpandsImmediateNonTerminalIntoParallelNonTerminals()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammarClosureCalculator calculator = new ContextFreeGrammarClosureCalculator();
+
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new NonTerminalNode("A"));
+        cfg.addProduction(new NonTerminalNode("A"), new NonTerminalNode("B"));
+        cfg.addProduction(new NonTerminalNode("B"), new NonTerminalNode("C"));
+
+        Set<ContextFreeGrammarSyntaxNode> productionSet = new HashSet<ContextFreeGrammarSyntaxNode>();
+        productionSet.add(this.buildProduction(new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+
+        Set<ContextFreeGrammarSyntaxNode> expectedProductionSet = new HashSet<ContextFreeGrammarSyntaxNode>();
+        expectedProductionSet.add(this.buildProduction(new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        expectedProductionSet.add(this.buildProduction(new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("B")));
+        expectedProductionSet.add(this.buildProduction(new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("C")));
+
+        assertEquals(expectedProductionSet, calculator.calculate(cfg, productionSet));
     }
 
     @Test
