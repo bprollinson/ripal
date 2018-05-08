6cb8898f8022db094bea327d92efa872954579fe
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-08 07:04:27 -0400

    Getting a a bunch of basic cfg closure calculator unit tests passing

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarClosureCalculator.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarClosureCalculator.java
@@ -3,12 +3,16 @@ package larp.syntaxcompiler.contextfreelanguage;
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class ContextFreeGrammarClosureCalculator
 {
     public Set<ContextFreeGrammarSyntaxNode> calculate(ContextFreeGrammar cfg, Set<ContextFreeGrammarSyntaxNode> productionSet)
     {
-        return null;
+        Set<ContextFreeGrammarSyntaxNode> productionsClosure = new HashSet<ContextFreeGrammarSyntaxNode>();
+        productionsClosure.addAll(productionSet);
+
+        return productionsClosure;
     }
 }
