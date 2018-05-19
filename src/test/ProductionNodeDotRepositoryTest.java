18272353d9d479d47ac87703652a468b890e7420
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-19 19:18:52 -0400

    Implementing unit test related to production node dot repository

--- src/test/ProductionNodeDotRepositoryTest.java
+++ src/test/ProductionNodeDotRepositoryTest.java
@@ -1,12 +1,30 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.parsetree.contextfreelanguage.ConcatenationNode;
+import larp.parsetree.contextfreelanguage.DotNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.ProductionNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+import larp.syntaxcompiler.contextfreelanguage.ProductionNodeDotRepository;
+
 public class ProductionNodeDotRepositoryTest
 {
     @Test
     public void testFindProductionSymbolAfterDotFindsProductionSymbolWhenDotPresent()
     {
-        assertEquals(0, 1);
+        ProductionNodeDotRepository repository = new ProductionNodeDotRepository();
+
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        concatenationNode.addChild(new DotNode());
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+
+        TerminalNode expectedNode = new TerminalNode("b");
+        assertEquals(expectedNode, repository.findProductionSymbolAfterDot(productionNode));
     }
 
     @Test
