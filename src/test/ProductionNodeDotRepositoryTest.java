ffff42da1b62bca44e678ee292edaa717b0f4239
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-19 12:36:33 -0400

    Adding placeholder unit tests for produciton node dot repository

--- src/test/ProductionNodeDotRepositoryTest.java
+++ src/test/ProductionNodeDotRepositoryTest.java
@@ -1,3 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class ProductionNodeDotRepositoryTest
 {
+    @Test
+    public void testFindProductionSymbolAfterDotFindsProductionSymbolWhenDotPresent()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testFindProductionSymbolAfterDotReturnsNullWhenDotNotPresent()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testAddDotToProductionRightHandSideAddsDot()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testShiftDotInProductionShiftsDot()
+    {
+        assertEquals(0, 1);
+    }
 }
