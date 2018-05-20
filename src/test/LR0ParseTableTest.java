d2dc51dbc24c83e224475c84238ae18f4b09a808
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-20 13:19:52 -0400

    Implementing first hack at unit test for LR0 parse table inequality based on different CFGs

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -1,9 +1,16 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parser.contextfreelanguage.LR0ParseTable;
+import larp.parser.contextfreelanguage.LR0ProductionSetDFAState;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+
+import java.util.HashSet;
 
 public class LR0ParseTableTest
 {
@@ -49,7 +56,21 @@ public class LR0ParseTableTest
     @Test
     public void testEqualsReturnsFalseForDifferentCFGs()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"));
+
+        ContextFreeGrammar otherCfg = new ContextFreeGrammar();
+        otherCfg.addProduction(new NonTerminalNode("S"), new TerminalNode("b"));
+
+        LR0ParseTable otherParseTable = new LR0ParseTable(otherCfg);
+        otherParseTable.addCell(state, new TerminalNode("a"));
+
+        assertFalse(parseTable.equals(otherParseTable));
     }
 
     @Test
