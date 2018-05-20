77e0c19a05417222d34047f8e8bc2a03ac0304cf
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-20 14:01:49 -0400

    Getting unit test forLR0 parse table inequality based on different CFGs passing

---
+++ src/main/larp/parser/contextfreelanguage/LR0ParseTable.java
@@ -0,0 +1,39 @@
+package larp.parser.contextfreelanguage;
+
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parser.regularlanguage.State;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+
+public class LR0ParseTable
+{
+    private ContextFreeGrammar contextFreeGrammar;
+
+    public LR0ParseTable(ContextFreeGrammar contextFreeGrammar)
+    {
+        this.contextFreeGrammar = contextFreeGrammar;
+    }
+
+    public void addCell(State state, ContextFreeGrammarSyntaxNode syntaxNode)
+    {
+    }
+
+    public ContextFreeGrammar getContextFreeGrammar()
+    {
+        return this.contextFreeGrammar;
+    }
+
+    public boolean equals(Object other)
+    {
+        if (!(other instanceof LR0ParseTable))
+        {
+            return false;
+        }
+
+        if (!this.contextFreeGrammar.equals(((LR0ParseTable)other).getContextFreeGrammar()))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
