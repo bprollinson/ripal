f5376e5681c8d2e71bfd1c3a1e566c90cd2ddcff
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-20 11:08:37 -0400

    Adding placeholder unit test methods for LR0 parse table cell addition

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -1,3 +1,29 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class LR0ParseTableTest
 {
+    @Test
+    public void testAddCellThrowsExceptionForTwoCellsWithTheSameStateAndSymbol()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testAddCellThrowsExceptionForTwoReduceActionsWithSameState()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testAddCellThrowsExceptionForShiftActionConflictingWithExistingReduceActionWithSameState()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testAddCellThrowsExceptionForReduceActionConflictingWithExistingShiftActionWithSameState()
+    {
+        assertEquals(0, 1);
+    }
 }
