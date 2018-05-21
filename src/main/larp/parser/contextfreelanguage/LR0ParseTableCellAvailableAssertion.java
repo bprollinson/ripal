5ee3acb40e4190310f0452394f242345ac9c83d6
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-21 11:02:55 -0400

    Indicating that the >LR0 parse table cell available assertion is an assertion via the assertion interface

--- src/main/larp/parser/contextfreelanguage/LR0ParseTableCellAvailableAssertion.java
+++ src/main/larp/parser/contextfreelanguage/LR0ParseTableCellAvailableAssertion.java
@@ -1,8 +1,9 @@
 package larp.parser.contextfreelanguage;
 
+import larp.assertion.Assertion;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 
-public class LR0ParseTableCellAvailableAssertion
+public class LR0ParseTableCellAvailableAssertion implements Assertion
 {
     public LR0ParseTableCellAvailableAssertion(LR0ParseTable parseTable, ContextFreeGrammarSyntaxNode syntaxNode, LR0ParseTableAction action)
     {
