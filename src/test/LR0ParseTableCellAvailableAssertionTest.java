ec52d85de80ed63bbe33eb2a6c8888f06d198d3a
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-22 07:17:12 -0400

    Adding another placeholder unit test for the LR0 parse table cell available assertion class

--- src/test/LR0ParseTableCellAvailableAssertionTest.java
+++ src/test/LR0ParseTableCellAvailableAssertionTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import larp.parser.contextfreelanguage.AmbiguousLR0ParseTableException;
 
@@ -7,5 +8,11 @@ public class LR0ParseTableCellAvailableAssertionTest
     @Test(expected = AmbiguousLR0ParseTableException.class)
     public void testValidateThrowsExceptionForShiftActionWhenCellContainsExistingShiftAction()
     {
+    }
+
+    @Test
+    public void testValidateDoesNotThrowExceptionForShiftActionWhenCellContainsTheSameStateAndDifferentSymbol()
+    {
+        assertEquals(0, 1);
     }
 }
