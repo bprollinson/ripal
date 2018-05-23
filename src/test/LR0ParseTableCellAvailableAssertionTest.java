93829bffcf4b6251344c5d8fe402a9c2aaad0a7e
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-23 07:16:33 -0400

    Implementing unit test for shift shift conflic in LR0 parse table cell available assertion test

--- src/test/LR0ParseTableCellAvailableAssertionTest.java
+++ src/test/LR0ParseTableCellAvailableAssertionTest.java
@@ -1,13 +1,33 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parser.contextfreelanguage.AmbiguousLR0ParseTableException;
+import larp.parser.contextfreelanguage.LR0ParseTable;
+import larp.parser.contextfreelanguage.LR0ParseTableCellAvailableAssertion;
+import larp.parser.contextfreelanguage.LR0ProductionSetDFAState;
+import larp.parser.contextfreelanguage.LR0ShiftAction;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+
+import java.util.HashSet;
 
 public class LR0ParseTableCellAvailableAssertionTest
 {
     @Test(expected = AmbiguousLR0ParseTableException.class)
     public void testValidateThrowsExceptionForShiftActionWhenCellContainsExistingShiftAction()
     {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ShiftAction(0));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, new TerminalNode("a"), new LR0ShiftAction(0));
+        assertion.validate();
     }
 
     @Test
