d213942f57b4afa381e04cdf8cb277f1fc8019e9
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-24 07:08:24 -0400

    Implementing unit test for shift reduce conflict in LR0 parse table

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -73,6 +73,14 @@ public class LR0ParseTableTest
     @Test(expected = AmbiguousLR0ParseTableException.class)
     public void testAddCellThrowsExceptionForShiftActionConflictingWithExistingReduceActionWithSameState()
     {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
+        parseTable.addCell(state, new TerminalNode("b"), new LR0ShiftAction(0));
     }
 
     @Test(expected = AmbiguousLR0ParseTableException.class)
