b0e5e00bbd751fe54d69aa460bb61e812fe7a243
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-23 21:57:00 -0400

    Implementing unit test for reduce reduce conflict in LR0 parse table cell available assertion class

--- src/test/LR0ParseTableCellAvailableAssertionTest.java
+++ src/test/LR0ParseTableCellAvailableAssertionTest.java
@@ -6,6 +6,7 @@ import larp.parser.contextfreelanguage.AmbiguousLR0ParseTableException;
 import larp.parser.contextfreelanguage.LR0ParseTable;
 import larp.parser.contextfreelanguage.LR0ParseTableCellAvailableAssertion;
 import larp.parser.contextfreelanguage.LR0ProductionSetDFAState;
+import larp.parser.contextfreelanguage.LR0ReduceAction;
 import larp.parser.contextfreelanguage.LR0ShiftAction;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 import larp.parsetree.contextfreelanguage.NonTerminalNode;
@@ -64,6 +65,16 @@ public class LR0ParseTableCellAvailableAssertionTest
     @Test(expected = AmbiguousLR0ParseTableException.class)
     public void testValidateThrowsExceptionForReduceActionWhenTableContainsReduceActionWithTheSameState()
     {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("b"), new LR0ReduceAction(1));
+        assertion.validate();
     }
 
     @Test(expected = AmbiguousLR0ParseTableException.class)
