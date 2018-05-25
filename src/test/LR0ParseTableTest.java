f328bad0173cd83dbc2659224c68ed5bfd08c909
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-25 07:10:05 -0400

    Adding unit test for non-trivial LR0 parse table equality

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -136,7 +136,21 @@ public class LR0ParseTableTest
     @Test
     public void testEqualsReturnsTrueForNonEmptyCFGAndTableEntries()
     {
-        assertEquals(0, 1);
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState otherState = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ShiftAction(0));
+        parseTable.addCell(otherState, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        LR0ParseTable otherParseTable = new LR0ParseTable(cfg);
+        otherParseTable.addCell(state, new TerminalNode("a"), new LR0ShiftAction(0));
+        otherParseTable.addCell(otherState, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        assertFalse(parseTable.equals(otherParseTable));
     }
 
     @Test
