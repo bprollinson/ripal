84c5e8bdaba864e7c500880fbf8152b43d2832a7
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-31 07:13:18 -0400

    Adding placeholder LR0 reduce action unit tests

--- src/test/LR0ReduceActionTest.java
+++ src/test/LR0ReduceActionTest.java
@@ -1,3 +1,23 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class LR0ReduceActionTest
 {
+    @Test
+    public void testEqualsReturnsTrueForReduceActionWithSameProductionIndex()
+    {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForReduceActionWithDifferentProductionIndex()
+    {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForActionOfOtherType()
+    {
+        assertTrue(false);
+    }
 }
