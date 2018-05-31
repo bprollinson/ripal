8907faceee890ef17307be29fc2df9b8beb2f82b
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-31 07:40:23 -0400

    Fillign in LR0 reduce action unit tests

--- src/test/LR0ReduceActionTest.java
+++ src/test/LR0ReduceActionTest.java
@@ -1,23 +1,36 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
+import larp.parser.contextfreelanguage.LR0ReduceAction;
+import larp.parser.contextfreelanguage.LR0ShiftAction;
 
 public class LR0ReduceActionTest
 {
     @Test
     public void testEqualsReturnsTrueForReduceActionWithSameProductionIndex()
     {
-        assertTrue(false);
+        LR0ReduceAction action = new LR0ReduceAction(0);
+        LR0ReduceAction otherAction = new LR0ReduceAction(0);
+
+        assertTrue(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForReduceActionWithDifferentProductionIndex()
     {
-        assertTrue(false);
+        LR0ReduceAction action = new LR0ReduceAction(0);
+        LR0ReduceAction otherAction = new LR0ReduceAction(1);
+
+        assertFalse(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForActionOfOtherType()
     {
-        assertTrue(false);
+        LR0ReduceAction action = new LR0ReduceAction(0);
+        LR0ShiftAction otherAction = new LR0ShiftAction(1);
+
+        assertFalse(action.equals(otherAction));
     }
 }
