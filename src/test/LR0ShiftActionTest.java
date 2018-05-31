310c756730805a58a47ee6ae6bfbc7b9a113be11
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-31 07:19:34 -0400

    Filling in LR0 shift action unit tests

--- src/test/LR0ShiftActionTest.java
+++ src/test/LR0ShiftActionTest.java
@@ -1,23 +1,36 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
+import larp.parser.contextfreelanguage.LR0ReduceAction;
+import larp.parser.contextfreelanguage.LR0ShiftAction;
 
 public class LR0ShiftActionTest
 {
     @Test
     public void testEqualsReturnsTrueForShiftActionWithSameStateIndex()
     {
-        assertTrue(false);
+        LR0ShiftAction action = new LR0ShiftAction(0);
+        LR0ShiftAction otherAction = new LR0ShiftAction(0);
+
+        assertTrue(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForShiftActionWithDifferentStateIndex()
     {
-        assertTrue(false);
+        LR0ShiftAction action = new LR0ShiftAction(0);
+        LR0ShiftAction otherAction = new LR0ShiftAction(1);
+
+        assertFalse(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForActionOfOtherType()
     {
-        assertTrue(false);
+        LR0ShiftAction action = new LR0ShiftAction(0);
+        LR0ReduceAction otherAction = new LR0ReduceAction(0);
+
+        assertFalse(action.equals(otherAction));
     }
 }
