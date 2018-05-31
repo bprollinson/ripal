7a7e87f7c6e125519f55af175b6ff2deaace9657
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-05-31 07:09:12 -0400

    Adding placeholder LR0 shift action unit tests

--- src/test/LR0ShiftActionTest.java
+++ src/test/LR0ShiftActionTest.java
@@ -1,3 +1,23 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class LR0ShiftActionTest
 {
+    @Test
+    public void testEqualsReturnsTrueForShiftActionWithSameStateIndex()
+    {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForShiftActionWithDifferentStateIndex()
+    {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForActionOfOtherType()
+    {
+        assertTrue(false);
+    }
 }
