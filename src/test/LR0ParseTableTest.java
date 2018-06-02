884e981df5ca4e15646e8ea37cec91b2cfc5aaa7
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-02 11:25:23 -0400

    Implementing unit test for LR0 parse table equality helper method

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -326,7 +326,18 @@ public class LR0ParseTableTest
     @Test
     public void testRowsEqualReturnsTrueWhenRowsMatchExpectedRows() throws AmbiguousLR0ParseTableException
     {
-        assertTrue(false);
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        Map<State, LR0ParseTableAction> otherRows = new HashMap<State, LR0ParseTableAction>();
+        otherRows.put(state, new LR0ReduceAction(0));
+
+        assertTrue(parseTable.rowsEqual(otherRows));
     }
 
     @Test
