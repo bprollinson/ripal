a4b57f4e51dd03e2c961d4a882e94d5629124929
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-03 15:35:28 -0400

    Implementing unit test for table row equality ignoring cells

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -424,7 +424,21 @@ public class LR0ParseTableTest
     @Test
     public void testRowsEqualOtherTableIgnoresCells() throws AmbiguousLR0ParseTableException
     {
-        assertTrue(false);
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState otherState = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
+        parseTable.addCell(otherState, new TerminalNode("a"), new LR0ShiftAction(0));
+
+        LR0ParseTable otherParseTable = new LR0ParseTable(cfg);
+        otherParseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
+        otherParseTable.addCell(otherState, new TerminalNode("a"), new LR0ShiftAction(1));
+
+        assertTrue(parseTable.rowsEqualOtherTable(otherParseTable));
     }
 
     @Test
