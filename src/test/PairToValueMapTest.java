8b2eaf5dc388c72181e2056d51e3ea3b4eb502a0
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-05 07:36:40 -0400

    Adding placeholder pair to value map unit tests

--- src/test/PairToValueMapTest.java
+++ src/test/PairToValueMapTest.java
@@ -1,3 +1,41 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class PairToValueMapTest
 {
+    @Test
+    public void testGetReturnsNullForNonExistentKeyPair()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetReturnsNullWhenOnlyFirstKeyMatchesSomeValue()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetReturnsNullWhenOnlySecondKeyMatchesSomeValue()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetReturnsValueForValidKeyPair()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetReturnsValueForNullFirstKey()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testGetReturnsValueForNullSecondKey()
+    {
+        assertEquals(0, 1);
+    }
 }
