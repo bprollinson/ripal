f89d68b529478cbd6afbc3fe2804fe31f37979fa
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-10 09:38:50 -0400

    Adding placeholder unit tests for LR0 goto action class

--- src/test/LR0GotoActionTest.java
+++ src/test/LR0GotoActionTest.java
@@ -1,3 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class LR0GotoActionTest
 {
+    @Test
+    public void testEqualsReturnsTrueForGotoActionWithSameStateIndex()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForGotoActionWithDifferentStateIndex()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForActionOfOtherType()
+    {
+        assertEquals(0, 1);
+    }
 }
