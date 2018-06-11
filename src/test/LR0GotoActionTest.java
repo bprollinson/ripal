3e42d388f091ced39437dfedb442c68f9c849352
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-11 07:13:03 -0400

    Implementing LR0 goto action class unit tests

--- src/test/LR0GotoActionTest.java
+++ src/test/LR0GotoActionTest.java
@@ -1,23 +1,36 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
+import larp.parser.contextfreelanguage.LR0GotoAction;
+import larp.parser.contextfreelanguage.LR0ShiftAction;
 
 public class LR0GotoActionTest
 {
     @Test
     public void testEqualsReturnsTrueForGotoActionWithSameStateIndex()
     {
-        assertEquals(0, 1);
+        LR0GotoAction action = new LR0GotoAction(0);
+        LR0GotoAction otherAction = new LR0GotoAction(0);
+
+        assertTrue(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForGotoActionWithDifferentStateIndex()
     {
-        assertEquals(0, 1);
+        LR0GotoAction action = new LR0GotoAction(0);
+        LR0GotoAction otherAction = new LR0GotoAction(1);
+
+        assertFalse(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForActionOfOtherType()
     {
-        assertEquals(0, 1);
+        LR0GotoAction action = new LR0GotoAction(0);
+        LR0ShiftAction otherAction = new LR0ShiftAction(1);
+
+        assertFalse(action.equals(otherAction));
     }
 }
