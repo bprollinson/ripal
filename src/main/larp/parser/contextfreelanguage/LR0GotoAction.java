8d9b3172af74005a9c609401e6039f660893cc75
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-11 19:42:36 -0400

    Getting LR0 goto action unit tests passing

--- src/main/larp/parser/contextfreelanguage/LR0GotoAction.java
+++ src/main/larp/parser/contextfreelanguage/LR0GotoAction.java
@@ -2,12 +2,30 @@ package larp.parser.contextfreelanguage;
 
 public class LR0GotoAction implements LR0ParseTableAction
 {
+    private int stateIndex;
+
     public LR0GotoAction(int stateIndex)
     {
+        this.stateIndex = stateIndex;
     }
 
     public boolean isRowLevelAction()
     {
         return false;
+    }
+
+    public int getStateIndex()
+    {
+        return this.stateIndex;
+    }
+
+    public boolean equals(Object other)
+    {
+        if (!(other instanceof LR0GotoAction))
+        {
+            return false;
+        }
+
+        return this.stateIndex == ((LR0GotoAction)other).getStateIndex();
     }
 }
