9bbfcd8cf1504767209702435a609b78bc5c9307
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-11 19:38:30 -0400

    Adding missing LR0 action classes

---
+++ src/main/larp/parser/contextfreelanguage/LR0AcceptAction.java
@@ -0,0 +1,9 @@
+package larp.parser.contextfreelanguage;
+
+public class LR0AcceptAction implements LR0ParseTableAction
+{
+    public boolean isRowLevelAction()
+    {
+        return false;
+    }
+}
---
+++ src/main/larp/parser/contextfreelanguage/LR0GotoAction.java
@@ -0,0 +1,13 @@
+package larp.parser.contextfreelanguage;
+
+public class LR0GotoAction implements LR0ParseTableAction
+{
+    public LR0GotoAction(int stateIndex)
+    {
+    }
+
+    public boolean isRowLevelAction()
+    {
+        return false;
+    }
+}
