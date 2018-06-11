a09ccc8f6dac5922c378bd429afb0feef0628f21
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-11 07:43:00 -0400

    Implementing LR0 accept action class unit tests

--- src/test/LR0AcceptActionTest.java
+++ src/test/LR0AcceptActionTest.java
@@ -1,17 +1,27 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+
+import larp.parser.contextfreelanguage.LR0AcceptAction;
+import larp.parser.contextfreelanguage.LR0ShiftAction;
 
 public class LR0AcceptActionTest
 {
     @Test
     public void testEqualsReturnsTrueForActionOfSameType()
     {
-        assertEquals(0, 1);
+        LR0AcceptAction action = new LR0AcceptAction();
+        LR0AcceptAction otherAction = new LR0AcceptAction();
+
+        assertTrue(action.equals(otherAction));
     }
 
     @Test
     public void testEqualsReturnsFalseForActionOfOtherType()
     {
-        assertEquals(0, 1);
+        LR0AcceptAction action = new LR0AcceptAction();
+        LR0ShiftAction otherAction = new LR0ShiftAction(0);
+
+        assertFalse(action.equals(otherAction));
     }
 }
