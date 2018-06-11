aacd946f2e0b174804da351cd0df8a9dbcea9a98
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-10 22:53:46 -0400

    Adding placeholder unit tests for LR0 accept action class

--- src/test/LR0AcceptActionTest.java
+++ src/test/LR0AcceptActionTest.java
@@ -1,3 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class LR0AcceptActionTest
 {
+    @Test
+    public void testEqualsReturnsTrueForActionOfSameType()
+    {
+        assertEquals(0, 1);
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForActionOfOtherType()
+    {
+        assertEquals(0, 1);
+    }
 }
