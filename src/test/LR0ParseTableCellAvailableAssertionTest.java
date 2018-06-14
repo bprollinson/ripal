c09d5b8e832e0dd30585a031fca8f303db25b7e6
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-14 19:20:47 -0400

    Reordering LR0 parse table cell available assertion unit tests

--- src/test/LR0ParseTableCellAvailableAssertionTest.java
+++ src/test/LR0ParseTableCellAvailableAssertionTest.java
@@ -113,27 +113,6 @@ public class LR0ParseTableCellAvailableAssertionTest
     }
 
     @Test(expected = AmbiguousLR0ParseTableException.class)
-    public void testValidateThrowsExceptionForReduceActionWhenTableContainsReduceForTheSameState() throws AmbiguousLR0ParseTableException
-    {
-        ContextFreeGrammar cfg = new ContextFreeGrammar();
-        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
-
-        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
-
-        LR0ParseTable parseTable = new LR0ParseTable(cfg);
-        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
-
-        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("b"), new LR0ReduceAction(1));
-        assertion.validate();
-    }
-
-    @Test
-    public void testValidateDoesNotThrowExceptionForReduceActionWhenTableContainsReduceActionForDifferentState()
-    {
-        throw new RuntimeException();
-    }
-
-    @Test(expected = AmbiguousLR0ParseTableException.class)
     public void testValidateThrowsExceptionForReduceActionWhenTableContainsShiftActionWithTheSameState() throws AmbiguousLR0ParseTableException
     {
         ContextFreeGrammar cfg = new ContextFreeGrammar();
@@ -162,6 +141,27 @@ public class LR0ParseTableCellAvailableAssertionTest
 
         LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, otherState, new TerminalNode("a"), new LR0ReduceAction(0));
         assertion.validate();
+    }
+
+    @Test(expected = AmbiguousLR0ParseTableException.class)
+    public void testValidateThrowsExceptionForReduceActionWhenTableContainsReduceForTheSameState() throws AmbiguousLR0ParseTableException
+    {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("b"), new LR0ReduceAction(1));
+        assertion.validate();
+    }
+
+    @Test
+    public void testValidateDoesNotThrowExceptionForReduceActionWhenTableContainsReduceActionForDifferentState()
+    {
+        throw new RuntimeException();
     }
 
     @Test(expected = AmbiguousLR0ParseTableException.class)
