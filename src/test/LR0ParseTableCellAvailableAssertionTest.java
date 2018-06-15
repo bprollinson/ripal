2f9cd0f8b2949da7e2b5ba12dc10f7b5d0372e42
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-14 22:22:10 -0400

    Implementing more LR0 parse table cell available assertion unit tests

--- src/test/LR0ParseTableCellAvailableAssertionTest.java
+++ src/test/LR0ParseTableCellAvailableAssertionTest.java
@@ -556,22 +556,50 @@ public class LR0ParseTableCellAvailableAssertionTest
         assertion.validate();
     }
 
-    @Test
-    public void testValidateThrowsExceptionForAcceptActionWhenTableContainsGotoForTheSameStateAndSymbol()
+    @Test(expected = AmbiguousLR0ParseTableException.class)
+    public void testValidateThrowsExceptionForAcceptActionWhenTableContainsGotoForTheSameStateAndSymbol() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0GotoAction(0));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("a"), new LR0AcceptAction());
+        assertion.validate();
     }
 
     @Test
-    public void testValidateDoesNotThrowExceptionForAcceptActionWhenTableContainsGotoForTheSameStateAndDifferentSymbol()
+    public void testValidateDoesNotThrowExceptionForAcceptActionWhenTableContainsGotoForTheSameStateAndDifferentSymbol() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0GotoAction(0));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("b"), new LR0AcceptAction());
+        assertion.validate();
     }
 
     @Test
-    public void testValidateDoesNotThrowExceptionForAcceptActionWhenTableContainsGotoForDifferentStateAndTheSameSymbol()
+    public void testValidateDoesNotThrowExceptionForAcceptActionWhenTableContainsGotoForDifferentStateAndTheSameSymbol() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState otherState = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0GotoAction(0));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, otherState, new TerminalNode("a"), new LR0AcceptAction());
+        assertion.validate();
     }
 
     @Test(expected = AmbiguousLR0ParseTableException.class)
