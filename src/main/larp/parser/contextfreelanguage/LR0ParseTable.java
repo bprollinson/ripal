b1905532173c76caee3e44ea949d1d227f426a12
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-17 10:35:04 -0400

    Partial unification of LR0 and LL1 parse table class logic

--- src/main/larp/parser/contextfreelanguage/LR0ParseTable.java
+++ src/main/larp/parser/contextfreelanguage/LR0ParseTable.java
@@ -5,59 +5,27 @@ import larp.parser.regularlanguage.State;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
 import larp.util.PairToValueMap;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LR0ParseTable
 {
     private ContextFreeGrammar contextFreeGrammar;
-    private Map<State, LR0ParseTableAction> rows;
-    private PairToValueMap<State, ContextFreeGrammarSyntaxNode, LR0ParseTableAction> cells;
+    private PairToValueMap<State, ContextFreeGrammarSyntaxNode, LR0ParseTableAction> table;
 
     public LR0ParseTable(ContextFreeGrammar contextFreeGrammar)
     {
         this.contextFreeGrammar = contextFreeGrammar;
-        this.rows = new HashMap<State, LR0ParseTableAction>();
-        this.cells = new PairToValueMap<State, ContextFreeGrammarSyntaxNode, LR0ParseTableAction>();
+        this.table = new PairToValueMap<State, ContextFreeGrammarSyntaxNode, LR0ParseTableAction>();
     }
 
     public void addCell(State state, ContextFreeGrammarSyntaxNode syntaxNode, LR0ParseTableAction action) throws AmbiguousLR0ParseTableException
     {
         new LR0ParseTableCellAvailableAssertion(this, state, syntaxNode, action).validate();
 
-        if (action.isRowLevelAction())
-        {
-            this.addRowEntry(state, action);
-        }
-        else
-        {
-            this.addCellEntry(state, syntaxNode, action);
-        }
-    }
-
-    private void addRowEntry(State state, LR0ParseTableAction action)
-    {
-        this.rows.put(state, action);
-    }
-
-    private void addCellEntry(State state, ContextFreeGrammarSyntaxNode syntaxNode, LR0ParseTableAction action)
-    {
-        this.cells.put(state, syntaxNode, action);
-    }
-
-    public LR0ParseTableAction getRow(State state)
-    {
-        return this.rows.get(state);
+        this.table.put(state, syntaxNode, action);
     }
 
     public LR0ParseTableAction getCell(State state, ContextFreeGrammarSyntaxNode syntaxNode)
     {
-        return this.cells.get(state, syntaxNode);
-    }
-
-    public boolean hasCellWithinRow(State state)
-    {
-        return this.cells.hasValueForFirstKey(state);
+        return this.table.get(state, syntaxNode);
     }
 
     public ContextFreeGrammar getContextFreeGrammar()
@@ -65,24 +33,9 @@ public class LR0ParseTable
         return this.contextFreeGrammar;
     }
 
-    public boolean rowsEqual(Map<State, LR0ParseTableAction> otherRows)
+    public PairToValueMap<State, ContextFreeGrammarSyntaxNode, LR0ParseTableAction> getTable()
     {
-        return this.rows.equals(otherRows);
-    }
-
-    public boolean rowsEqualOtherTable(LR0ParseTable otherTable)
-    {
-        return otherTable.rowsEqual(this.rows);
-    }
-
-    public boolean cellsEqual(PairToValueMap<State, ContextFreeGrammarSyntaxNode, LR0ParseTableAction> otherCells)
-    {
-        return this.cells.equals(otherCells);
-    }
-
-    public boolean cellsEqualOtherTable(LR0ParseTable otherTable)
-    {
-        return otherTable.cellsEqual(this.cells);
+        return this.table;
     }
 
     public boolean equals(Object other)
@@ -97,6 +50,6 @@ public class LR0ParseTable
             return false;
         }
 
-        return this.cellsEqualOtherTable((LR0ParseTable)other) && this.rowsEqualOtherTable((LR0ParseTable)other);
+        return this.table.equals(((LR0ParseTable)other).getTable());
     }
 }
