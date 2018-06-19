72989b7c11b6207c721009daabdd1ebbe189b0c3
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-06-18 20:13:44 -0400

    Implementing first LR0 syyntax compiler unit test

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0SyntaxCompiler.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0SyntaxCompiler.java
@@ -1,5 +1,12 @@
 package larp.syntaxcompiler.contextfreelanguage;
 
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parser.contextfreelanguage.LR0ParseTable;
+
 public class ContextFreeGrammarLR0SyntaxCompiler
 {
+    public LR0ParseTable compile(ContextFreeGrammar grammar)
+    {
+        return null;
+    }
 }
--- src/test/ContextFreeGrammarLR0SyntaxCompilerTest.java
+++ src/test/ContextFreeGrammarLR0SyntaxCompilerTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parser.contextfreelanguage.LR0ParseTable;
 import larp.syntaxcompiler.contextfreelanguage.ContextFreeGrammarLR0SyntaxCompiler;
 
 public class ContextFreeGrammarLR0SyntaxCompilerTest
@@ -9,6 +11,11 @@ public class ContextFreeGrammarLR0SyntaxCompilerTest
     public void testCompileReturnsEmptyParseTableForEmptyCFG()
     {
         ContextFreeGrammarLR0SyntaxCompiler compiler = new ContextFreeGrammarLR0SyntaxCompiler();
+
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+        LR0ParseTable expectedTable = new LR0ParseTable(grammar);
+
+        assertEquals(expectedTable, compiler.compile(grammar));
     }
 
     @Test
