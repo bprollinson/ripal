ed38211308838ecbd4f704e529d5f5ac6bfa1c2d
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-08-07 21:08:29 -0400

    Implementing LR0 parse table unit test

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -332,9 +332,23 @@ public class LR0ParseTableTest
     }
 
     @Test
-    public void testStructureEqualsReturnsTrueForAnalogousGotoAction()
+    public void testStructureEqualsReturnsTrueForAnalogousGotoAction() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+
+        LR0ProductionSetDFAState state1 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState state2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ProductionSetDFAState otherState1 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState otherState2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg);
+        parseTable.addCell(state1, new TerminalNode("a"), new LR0GotoAction(state2));
+
+        LR0ParseTable otherParseTable = new LR0ParseTable(cfg);
+        otherParseTable.addCell(otherState1, new TerminalNode("a"), new LR0GotoAction(otherState2));
+
+        assertTrue(parseTable.structureEquals(otherParseTable));
     }
 
     @Test
