3cb11fde869e39fd731e8d339b3389815dbdb1a7
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-09-29 00:58:47 -0400

    Implementing LR0 parse table unit test

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -490,9 +490,25 @@ public class LR0ParseTableTest
     }
 
     @Test
-    public void testStructureEqualsReturnsFalseForDifferentActionInstanceAfterGotoAction()
+    public void testStructureEqualsReturnsFalseForDifferentActionInstanceAfterGotoAction() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+
+        LR0ProductionSetDFAState state1 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState state2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ProductionSetDFAState otherState1 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState otherState2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg, state1);
+        parseTable.addCell(state1, new TerminalNode("a"), new LR0GotoAction(state2));
+        parseTable.addCell(state2, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        LR0ParseTable otherParseTable = new LR0ParseTable(cfg, otherState1);
+        otherParseTable.addCell(otherState1, new TerminalNode("a"), new LR0GotoAction(otherState2));
+        otherParseTable.addCell(otherState2, new TerminalNode("a"), new LR0ReduceAction(1));
+
+        assertFalse(parseTable.structureEquals(otherParseTable));
     }
 
     @Test
