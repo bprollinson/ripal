5ca9cf022ae708377238edec61cfdcf9758783bf
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-03 21:00:59 -0400

    Adding another LR0 parse table structureEquals unit test

--- src/test/LR0ParseTableTest.java
+++ src/test/LR0ParseTableTest.java
@@ -342,6 +342,23 @@ public class LR0ParseTableTest
     }
 
     @Test
+    public void testStructureEqualsReturnsFalseForActionWithDifferentInputBetweenTables() throws AmbiguousLR0ParseTableException
+    {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+
+        LR0ProductionSetDFAState state1 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState state2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(cfg, state1);
+        parseTable.addCell(state1, new TerminalNode("a"), new LR0ReduceAction(0));
+
+        LR0ParseTable otherParseTable = new LR0ParseTable(cfg, state1);
+        otherParseTable.addCell(state1, new TerminalNode("b"), new LR0ReduceAction(0));
+
+        assertFalse(parseTable.structureEquals(otherParseTable));
+    }
+
+    @Test
     public void testStructureEqualsReturnsFalseForDifferentActionTypeBetweenTables() throws AmbiguousLR0ParseTableException
     {
         ContextFreeGrammar cfg = new ContextFreeGrammar();
