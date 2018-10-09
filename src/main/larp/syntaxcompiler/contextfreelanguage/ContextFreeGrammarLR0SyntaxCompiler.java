974724baa3b8a0a1f4a6d5ff31c3f08d14d819bc
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-08 20:57:59 -0400

    Setting up some of the basic infrastructure within LR0 syntax compiler

--- src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0SyntaxCompiler.java
+++ src/main/larp/syntaxcompiler/contextfreelanguage/ContextFreeGrammarLR0SyntaxCompiler.java
@@ -2,11 +2,26 @@ package larp.syntaxcompiler.contextfreelanguage;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parser.contextfreelanguage.LR0ParseTable;
+import larp.parser.contextfreelanguage.LR0ProductionSetDFA;
 
 public class ContextFreeGrammarLR0SyntaxCompiler
 {
+    private ContextFreeGrammarLR0ProductionSetDFACompiler DFACompiler;
+
+    public ContextFreeGrammarLR0SyntaxCompiler()
+    {
+        this.DFACompiler = new ContextFreeGrammarLR0ProductionSetDFACompiler();
+    }
+
     public LR0ParseTable compile(ContextFreeGrammar grammar)
     {
-        return new LR0ParseTable(grammar, null);
+        if (grammar.getStartSymbol() == null)
+        {
+            return new LR0ParseTable(grammar, null);
+        }
+
+        LR0ProductionSetDFA DFA = this.DFACompiler.compile(grammar);
+
+        return new LR0ParseTable(grammar, DFA.getStartState());
     }
 }
