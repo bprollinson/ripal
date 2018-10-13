e03ce6f6b2b981e0dc855934d03a2318704b329c
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-13 13:25:53 -0400

    Correcting LR0 parse table construction unit test

--- src/test/ContextFreeGrammarLR0SyntaxCompilerTest.java
+++ src/test/ContextFreeGrammarLR0SyntaxCompilerTest.java
@@ -308,15 +308,19 @@ public class ContextFreeGrammarLR0SyntaxCompilerTest
         LR0ProductionSetDFAState state4 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
         LR0ProductionSetDFAState state5 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
 
-        LR0ParseTable expectedTable = new LR0ParseTable(grammar, state1);
+        ContextFreeGrammar augmentedGrammar = new ContextFreeGrammar();
+        augmentedGrammar.addProduction(new NonTerminalNode("S'"), new NonTerminalNode("S"), new EndOfStringNode());
+        augmentedGrammar.addProduction(new NonTerminalNode("S"), new TerminalNode("a"), new TerminalNode("b"), new TerminalNode("c"));
+
+        LR0ParseTable expectedTable = new LR0ParseTable(augmentedGrammar, state1);
         expectedTable.addCell(state1, new TerminalNode("a"), new LR0ShiftAction(state2));
         expectedTable.addCell(state1, new NonTerminalNode("S"), new LR0GotoAction(state5));
         expectedTable.addCell(state2, new TerminalNode("b"), new LR0ShiftAction(state3));
         expectedTable.addCell(state3, new TerminalNode("c"), new LR0ShiftAction(state4));
-        expectedTable.addCell(state4, new TerminalNode("a"), new LR0ReduceAction(2));
-        expectedTable.addCell(state4, new TerminalNode("b"), new LR0ReduceAction(2));
-        expectedTable.addCell(state4, new TerminalNode("c"), new LR0ReduceAction(2));
-        expectedTable.addCell(state4, new EndOfStringNode(), new LR0ReduceAction(2));
+        expectedTable.addCell(state4, new TerminalNode("a"), new LR0ReduceAction(1));
+        expectedTable.addCell(state4, new TerminalNode("b"), new LR0ReduceAction(1));
+        expectedTable.addCell(state4, new TerminalNode("c"), new LR0ReduceAction(1));
+        expectedTable.addCell(state4, new EndOfStringNode(), new LR0ReduceAction(1));
         expectedTable.addCell(state5, new EndOfStringNode(), new LR0AcceptAction());
 
         assertTrue(expectedTable.structureEquals(compiler.compile(grammar)));
