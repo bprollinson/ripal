50fbc7cb97d391309e897ce510a1f47e16c11df1
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-14 14:04:38 -0400

    Setting up base part of unit test required to assert that LR0 parser class exists, creating LR0 parser class

---
+++ src/main/larp/parser/contextfreelanguage/LR0Parser.java
@@ -0,0 +1,8 @@
+package larp.parser.contextfreelanguage;
+
+public class LR0Parser
+{
+    public LR0Parser(LR0ParseTable parseTable)
+    {
+    }
+}
--- src/test/LR0ParserTest.java
+++ src/test/LR0ParserTest.java
@@ -1,10 +1,23 @@
 import org.junit.Test;
 
+import larp.grammar.contextfreelanguage.ContextFreeGrammar;
+import larp.parser.contextfreelanguage.LR0Parser;
+import larp.parser.contextfreelanguage.LR0ParseTable;
+import larp.parser.contextfreelanguage.LR0ProductionSetDFAState;
+import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+
+import java.util.HashSet;
+
 public class LR0ParserTest
 {
     @Test
     public void testStructureEqualsReturnsTrueWhenParseTablesHaveSameStructure()
     {
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        LR0ProductionSetDFAState startState = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ParseTable parseTable = new LR0ParseTable(cfg, startState);
+        LR0Parser parser = new LR0Parser(parseTable);
+
         throw new RuntimeException();
     }
 
