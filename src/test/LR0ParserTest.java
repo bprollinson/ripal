347db9e96ef75c8f40a7df23ca3998a271534823
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-15 19:40:12 -0400

    Implementing LR0 parse table structureEquals unit test

--- src/test/LR0ParserTest.java
+++ src/test/LR0ParserTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -38,8 +39,24 @@ public class LR0ParserTest
     }
 
     @Test
-    public void testStructureEqualsReturnsFalseWhenParseTablesHaveDifferentStructure()
+    public void testStructureEqualsReturnsFalseWhenParseTablesHaveDifferentStructure() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        ContextFreeGrammar cfg = new ContextFreeGrammar();
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+        cfg.addProduction(new NonTerminalNode("S"), new TerminalNode("b"));
+
+        LR0ProductionSetDFAState startState = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ParseTable parseTable = new LR0ParseTable(cfg, startState);
+        parseTable.addCell(startState, new TerminalNode("a"), new LR0ReduceAction(0));
+        parseTable.addCell(startState, new TerminalNode("b"), new LR0ReduceAction(1));
+        LR0Parser parser = new LR0Parser(parseTable);
+
+        LR0ProductionSetDFAState otherStartState = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ParseTable otherParseTable = new LR0ParseTable(cfg, otherStartState);
+        otherParseTable.addCell(otherStartState, new TerminalNode("a"), new LR0ReduceAction(0));
+        otherParseTable.addCell(otherStartState, new TerminalNode("b"), new LR0ReduceAction(0));
+        LR0Parser otherParser = new LR0Parser(otherParseTable);
+
+        assertFalse(parser.structureEquals(otherParser));
     }
 }
