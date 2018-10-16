2cd371b0315dac0da558af4b2be8f3ca6d01b3db
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-16 07:04:58 -0400

    Getting LR0 parse table structureEquals unit tests passing

--- src/main/larp/parser/contextfreelanguage/LR0Parser.java
+++ src/main/larp/parser/contextfreelanguage/LR0Parser.java
@@ -4,12 +4,25 @@ import larp.ComparableStructure;
 
 public class LR0Parser implements ComparableStructure
 {
+    private LR0ParseTable parseTable;
+
     public LR0Parser(LR0ParseTable parseTable)
     {
+        this.parseTable = parseTable;
+    }
+
+    public LR0ParseTable getParseTable()
+    {
+        return this.parseTable;
     }
 
     public boolean structureEquals(Object other)
     {
-        return false;
+        if (!(other instanceof LR0Parser))
+        {
+            return false;
+        }
+
+        return this.parseTable.structureEquals(((LR0Parser)other).getParseTable());
     }
 }
