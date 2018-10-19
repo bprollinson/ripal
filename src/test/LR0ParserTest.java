c7fc3eef6dde7507416044b65f8572ab2a5fcba9
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-18 20:50:06 -0400

    Implementing unit test for extra input character handing in LR0 parser

--- src/test/LR0ParserTest.java
+++ src/test/LR0ParserTest.java
@@ -125,9 +125,26 @@ public class LR0ParserTest
     }
 
     @Test
-    public void testAcceptsReturnsFalseForUnmatchedInputCharacter()
+    public void testAcceptsReturnsFalseForUnmatchedInputCharacter() throws AmbiguousLR0ParseTableException
     {
-        throw new RuntimeException();
+        LR0ProductionSetDFAState state1 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState state2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState state3 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        ContextFreeGrammar augmentedGrammar = new ContextFreeGrammar();
+        augmentedGrammar.addProduction(new NonTerminalNode("S'"), new NonTerminalNode("S"), new EndOfStringNode());
+        augmentedGrammar.addProduction(new NonTerminalNode("S"), new TerminalNode("a"));
+
+        LR0ParseTable parseTable = new LR0ParseTable(augmentedGrammar, state1);
+        parseTable.addCell(state1, new TerminalNode("a"), new LR0ShiftAction(state2));
+        parseTable.addCell(state1, new NonTerminalNode("S"), new LR0GotoAction(state3));
+        parseTable.addCell(state2, new TerminalNode("a"), new LR0ReduceAction(1));
+        parseTable.addCell(state2, new EndOfStringNode(), new LR0ReduceAction(1));
+        parseTable.addCell(state3, new EndOfStringNode(), new LR0AcceptAction());
+
+        LR0Parser parser = new LR0Parser(parseTable);
+
+        assertFalse(parser.accepts("ab"));
     }
 
     @Test
