fa87f25edfaa28e8d84531b27875b68a7d46d337
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-10-26 20:07:10 -0400

    Enriching context-free grammar parser interface

--- src/main/larp/parser/contextfreelanguage/ContextFreeGrammarParser.java
+++ src/main/larp/parser/contextfreelanguage/ContextFreeGrammarParser.java
@@ -1,5 +1,9 @@
 package larp.parser.contextfreelanguage;
 
+import java.util.List;
+
 public interface ContextFreeGrammarParser
 {
+    public boolean accepts(String inputString);
+    public List<Integer> getAppliedRules();
 }
