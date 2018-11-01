ed8584b17560078ed62808a87de6bc14afeacece
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-11-01 07:21:01 -0400

    Adding placeholder unit tests for the LR0 parse stack object

--- src/test/LR0ParseStackTest.java
+++ src/test/LR0ParseStackTest.java
@@ -1,3 +1,58 @@
+import org.junit.Test;
+
 public class LR0ParseStackTest
 {
+    @Test
+    public void testPeekReturnsTopObject()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testPeekThrowsExceptionWhenStackIsEmpty()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testPopReturnsAndRemovesTopObject()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testPopThrowsExceptionWhenStackIsEmpty()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testGetTopStateReturnsTopElement()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testGetTopStateReturnsLowerElement()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testGetTopStateThrowsExceptionWhenStateNotFound()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testEqualsReturnsTrueForStacksContainingSameObjects()
+    {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void testEqualsReturnsFalseForStacksContainingDifferentObjects()
+    {
+        throw new RuntimeException();
+    }
 }
