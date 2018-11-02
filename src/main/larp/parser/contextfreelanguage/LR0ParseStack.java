1865f3f6d17ba53c6766eb5e386618d44d35cef8
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-11-01 21:22:39 -0400

    Implementing simple LR0 parse stack operations

--- src/main/larp/parser/contextfreelanguage/LR0ParseStack.java
+++ src/main/larp/parser/contextfreelanguage/LR0ParseStack.java
@@ -2,24 +2,49 @@ package larp.parser.contextfreelanguage;
 
 import larp.parser.regularlanguage.State;
 
+import java.util.Stack;
+
 public class LR0ParseStack
 {
+    private Stack<Object> stack;
+
+    public LR0ParseStack()
+    {
+        this.stack = new Stack<Object>();
+    }
+
     public void push(Object stackEntry)
     {
+        this.stack.push(stackEntry);
     }
 
     public Object peek()
     {
-        return null;
+        return this.stack.peek();
     }
 
     public Object pop()
     {
-        return null;
+        return this.stack.pop();
     }
 
     public State getTopState()
     {
         return null;
+    }
+
+    public Stack<Object> getStack()
+    {
+        return this.stack;
+    }
+
+    public boolean equals(Object other)
+    {
+        if (!(other instanceof LR0ParseStack))
+        {
+            return false;
+        }
+
+        return this.stack.equals(((LR0ParseStack)other).getStack());
     }
 }
