7fbf1f7f682accc87c3dc75260645e330d5fff17
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-11-02 20:26:12 -0400

    Checking for grammar equality in LR0 production set DFA structural equality

--- src/main/larp/parser/contextfreelanguage/LR0ProductionSetDFA.java
+++ src/main/larp/parser/contextfreelanguage/LR0ProductionSetDFA.java
@@ -6,8 +6,27 @@ import larp.parser.regularlanguage.State;
 
 public class LR0ProductionSetDFA extends FiniteAutomata<LR0ProductionSetDFAState>
 {
+    private ContextFreeGrammar grammar;
+
     public LR0ProductionSetDFA(LR0ProductionSetDFAState startState, ContextFreeGrammar grammar)
     {
         super(startState);
+
+        this.grammar = grammar;
+    }
+
+    public ContextFreeGrammar getGrammar()
+    {
+        return this.grammar;
+    }
+
+    public boolean structureEquals(Object other)
+    {
+        if (!super.structureEquals(other))
+        {
+            return false;
+        }
+
+        return this.grammar.equals(((LR0ProductionSetDFA)other).getGrammar());
     }
 }
