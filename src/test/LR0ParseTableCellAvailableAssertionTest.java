dbd822c44f4fd4712bcf55f6f4f1dd9c10d6bf6d
Brendan Rollinson <brendan.rollinson@gmail.com>
2018-11-04 22:03:07 -0500

    Implementing LR0 parse table cell available assertion unit tests for different exception types

--- src/test/LR0ParseTableCellAvailableAssertionTest.java
+++ src/test/LR0ParseTableCellAvailableAssertionTest.java
@@ -9,14 +9,17 @@ import org.junit.Test;
 
 import larp.grammar.contextfreelanguage.ContextFreeGrammar;
 import larp.parser.contextfreelanguage.AmbiguousLR0ParseTableException;
+import larp.parser.contextfreelanguage.LR0GotoAction;
 import larp.parser.contextfreelanguage.LR0OtherConflictException;
 import larp.parser.contextfreelanguage.LR0ParseTable;
 import larp.parser.contextfreelanguage.LR0ParseTableCellAvailableAssertion;
 import larp.parser.contextfreelanguage.LR0ProductionSetDFAState;
+import larp.parser.contextfreelanguage.LR0ReduceAction;
 import larp.parser.contextfreelanguage.LR0ReduceReduceConflictException;
 import larp.parser.contextfreelanguage.LR0ShiftAction;
 import larp.parser.contextfreelanguage.LR0ShiftReduceConflictException;
 import larp.parsetree.contextfreelanguage.ContextFreeGrammarSyntaxNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
 import larp.parsetree.contextfreelanguage.TerminalNode;
 
 import java.util.HashSet;
@@ -24,34 +27,59 @@ import java.util.HashSet;
 public class LR0ParseTableCellAvailableAssertionTest
 {
     @Test(expected = LR0ShiftReduceConflictException.class)
-    public void testValidateThrowsExceptionForShiftActionWhenCellContainsReduceAction()
+    public void testValidateThrowsExceptionForShiftActionWhenCellContainsReduceAction() throws AmbiguousLR0ParseTableException
     {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(grammar, null);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(1));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("a"), new LR0ShiftAction(state));
+        assertion.validate();
     }
 
     @Test(expected = LR0ShiftReduceConflictException.class)
-    public void testValidateThrowsExceptionForReduceActionWhenCellContainsShiftAction()
+    public void testValidateThrowsExceptionForReduceActionWhenCellContainsShiftAction() throws AmbiguousLR0ParseTableException
     {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(grammar, null);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ShiftAction(state));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("a"), new LR0ReduceAction(1));
+        assertion.validate();
     }
 
     @Test(expected = LR0ReduceReduceConflictException.class)
-    public void testValidateThrowsExceptionForReduceActionWhenCellContainsReduceAction()
+    public void testValidateThrowsExceptionForReduceActionWhenCellContainsReduceAction() throws AmbiguousLR0ParseTableException
     {
+        ContextFreeGrammar grammar = new ContextFreeGrammar();
+
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(grammar, null);
+        parseTable.addCell(state, new TerminalNode("a"), new LR0ReduceAction(1));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("a"), new LR0ReduceAction(1));
+        assertion.validate();
     }
 
     @Test(expected = LR0OtherConflictException.class)
-    public void testValidateThrowsExceptionForOtherTypeOfConflict()
-    {
-    }
-
-    @Test(expected = AmbiguousLR0ParseTableException.class)
-    public void testValidateThrowsExceptionForCellThatAlreadyExists() throws AmbiguousLR0ParseTableException
+    public void testValidateThrowsExceptionForOtherTypeOfConflict() throws AmbiguousLR0ParseTableException
     {
         ContextFreeGrammar grammar = new ContextFreeGrammar();
-        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
-        LR0ParseTable parseTable = new LR0ParseTable(grammar, null);
-        parseTable.addCell(state, new TerminalNode("a"), new LR0ShiftAction(state));
-        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new TerminalNode("a"), new LR0ShiftAction(state));
 
+        LR0ProductionSetDFAState state = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+        LR0ProductionSetDFAState state2 = new LR0ProductionSetDFAState("", false, new HashSet<ContextFreeGrammarSyntaxNode>());
+
+        LR0ParseTable parseTable = new LR0ParseTable(grammar, null);
+        parseTable.addCell(state, new NonTerminalNode("A"), new LR0GotoAction(state2));
+
+        LR0ParseTableCellAvailableAssertion assertion = new LR0ParseTableCellAvailableAssertion(parseTable, state, new NonTerminalNode("A"), new LR0GotoAction(state2));
         assertion.validate();
     }
 
