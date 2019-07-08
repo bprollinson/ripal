f8e05368cf477416420b73956344a493e231b289
Brendan Rollinson <brendan.rollinson@gmail.com>
2019-07-07 20:22:59 -0400

    Implementing unit tests for grammar closure rule equality

--- src/test/larp/parsercompiler/contextfreelanguage/GrammarClosureRuleTest.java
+++ src/test/larp/parsercompiler/contextfreelanguage/GrammarClosureRuleTest.java
@@ -8,25 +8,100 @@
 package larp.parsercompiler.contextfreelanguage;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import org.junit.Test;
+
+import larp.parsetree.contextfreelanguage.ConcatenationNode;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.Node;
+import larp.parsetree.contextfreelanguage.ProductionNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+
+import java.util.HashSet;
 
 public class GrammarClosureRuleTest
 {
     @Test
     public void testEqualsReturnsTrueForRuleWithSameSymbolsAndLookaheads()
     {
-        assertEquals(0, 1);
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+
+        HashSet<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+
+        GrammarClosureRule rule = new GrammarClosureRule(productionNode, lookaheadSymbols);
+
+        ProductionNode otherProductionNode = new ProductionNode();
+        otherProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode otherConcatenationNode = new ConcatenationNode();
+        otherConcatenationNode.addChild(new TerminalNode("a"));
+        otherProductionNode.addChild(otherConcatenationNode);
+
+        HashSet<Node> otherLookaheadSymbols = new HashSet<Node>();
+        otherLookaheadSymbols.add(new TerminalNode("b"));
+
+        GrammarClosureRule otherRule = new GrammarClosureRule(otherProductionNode, otherLookaheadSymbols);
+
+        assertEquals(otherRule, rule);
     }
 
     @Test
     public void testEqualsReturnsFalseForDifferentSymbols()
     {
-        assertEquals(0, 1);
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+
+        HashSet<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+
+        GrammarClosureRule rule = new GrammarClosureRule(productionNode, lookaheadSymbols);
+
+        ProductionNode otherProductionNode = new ProductionNode();
+        otherProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode otherConcatenationNode = new ConcatenationNode();
+        otherConcatenationNode.addChild(new TerminalNode("b"));
+        otherProductionNode.addChild(otherConcatenationNode);
+
+        HashSet<Node> otherLookaheadSymbols = new HashSet<Node>();
+        otherLookaheadSymbols.add(new TerminalNode("b"));
+
+        GrammarClosureRule otherRule = new GrammarClosureRule(otherProductionNode, otherLookaheadSymbols);
+
+        assertNotEquals(otherRule, rule);
     }
 
     @Test
     public void testNotEqualsReturnsFalseForDIfferentLookaheads()
     {
-        assertEquals(0, 1);
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+
+        HashSet<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+
+        GrammarClosureRule rule = new GrammarClosureRule(productionNode, lookaheadSymbols);
+
+        ProductionNode otherProductionNode = new ProductionNode();
+        otherProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode otherConcatenationNode = new ConcatenationNode();
+        otherConcatenationNode.addChild(new TerminalNode("a"));
+        otherProductionNode.addChild(otherConcatenationNode);
+
+        HashSet<Node> otherLookaheadSymbols = new HashSet<Node>();
+        otherLookaheadSymbols.add(new TerminalNode("c"));
+
+        GrammarClosureRule otherRule = new GrammarClosureRule(otherProductionNode, otherLookaheadSymbols);
+
+        assertNotEquals(otherRule, rule);
     }
 }
