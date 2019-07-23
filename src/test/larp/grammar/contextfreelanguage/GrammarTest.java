a781720ab5691b05d9bdf498bfe5909736eeb430
Brendan Rollinson <brendan.rollinson@gmail.com>
2019-07-22 22:06:47 -0400

    Adding unit tests for finding production indexes in a context-free grammar

--- src/test/larp/grammar/contextfreelanguage/GrammarTest.java
+++ src/test/larp/grammar/contextfreelanguage/GrammarTest.java
@@ -13,9 +13,12 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
+import larp.parsetree.contextfreelanguage.ConcatenationNode;
 import larp.parsetree.contextfreelanguage.NonTerminalNode;
 import larp.parsetree.contextfreelanguage.ProductionNode;
 import larp.parsetree.contextfreelanguage.TerminalNode;
+
+import java.util.ArrayList;
 
 public class GrammarTest
 {
@@ -41,6 +44,90 @@ public class GrammarTest
         Grammar grammar = new Grammar();
 
         assertNull(grammar.getStartSymbol());
+    }
+
+    @Test
+    public void testFindProductionPositionsReturnsEmptySet()
+    {
+        Grammar grammar = new Grammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        ProductionNode expectedProductionNode = new ProductionNode();
+        expectedProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode expectedConcatenationNode = new ConcatenationNode();
+        expectedConcatenationNode.addChild(new TerminalNode("b"));
+        expectedProductionNode.addChild(expectedConcatenationNode);
+
+        ArrayList<Integer> expectedPositions = new ArrayList<Integer>();
+        assertEquals(expectedPositions, grammar.findProductionPositions(expectedProductionNode));
+    }
+
+    @Test
+    public void testFindProductionPositionsReturnsSinglePosition()
+    {
+        Grammar grammar = new Grammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        ProductionNode expectedProductionNode = new ProductionNode();
+        expectedProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode expectedConcatenationNode = new ConcatenationNode();
+        expectedConcatenationNode.addChild(new TerminalNode("b"));
+        expectedProductionNode.addChild(expectedConcatenationNode);
+
+        ArrayList<Integer> expectedPositions = new ArrayList<Integer>();
+        expectedPositions.add(1);
+        assertEquals(expectedPositions, grammar.findProductionPositions(expectedProductionNode));
+    }
+
+    @Test
+    public void testFindProductionPositionsReturnsMultiplePositions()
+    {
+        Grammar grammar = new Grammar();
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+        productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("b"));
+        productionNode.addChild(concatenationNode);
+        grammar.addProduction(productionNode);
+
+        ProductionNode expectedProductionNode = new ProductionNode();
+        expectedProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode expectedConcatenationNode = new ConcatenationNode();
+        expectedConcatenationNode.addChild(new TerminalNode("b"));
+        expectedProductionNode.addChild(expectedConcatenationNode);
+
+        ArrayList<Integer> expectedPositions = new ArrayList<Integer>();
+        expectedPositions.add(1);
+        expectedPositions.add(2);
+        assertEquals(expectedPositions, grammar.findProductionPositions(expectedProductionNode));
     }
 
     @Test
