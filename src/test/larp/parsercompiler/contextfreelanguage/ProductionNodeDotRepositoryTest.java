3b716875d6123161be65071666087b41c3e5cb95
Brendan Rollinson <brendan.rollinson@gmail.com>
2019-07-24 21:23:14 -0400

    Adding missing production node dot repository unit test

--- src/test/larp/parsercompiler/contextfreelanguage/ProductionNodeDotRepositoryTest.java
+++ src/test/larp/parsercompiler/contextfreelanguage/ProductionNodeDotRepositoryTest.java
@@ -99,6 +99,29 @@ public class ProductionNodeDotRepositoryTest
     }
 
     @Test
+    public void testShiftDotInProductionRemovedTrailingDot()
+    {
+        ProductionNodeDotRepository repository = new ProductionNodeDotRepository();
+
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        concatenationNode.addChild(new TerminalNode("a"));
+        concatenationNode.addChild(new TerminalNode("b"));
+        concatenationNode.addChild(new DotNode());
+        productionNode.addChild(concatenationNode);
+
+        ProductionNode expectedProductionNode = new ProductionNode();
+        expectedProductionNode.addChild(new NonTerminalNode("S"));
+        ConcatenationNode expectedConcatenationNode = new ConcatenationNode();
+        expectedConcatenationNode.addChild(new TerminalNode("a"));
+        expectedConcatenationNode.addChild(new TerminalNode("b"));
+        expectedProductionNode.addChild(expectedConcatenationNode);
+
+        assertEquals(expectedProductionNode, repository.shiftDotInProduction(productionNode));
+    }
+
+    @Test
     public void testShiftDotInProductionDoesNothingWhenDotNotFound()
     {
         ProductionNodeDotRepository repository = new ProductionNodeDotRepository();
