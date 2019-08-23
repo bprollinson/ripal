b43507541baa3c37294f55fa1990b4565bc08dfe
Brendan Rollinson <brendan.rollinson@gmail.com>
2019-08-22 22:39:01 -0400

    Implementing unit tests for closure lookahead combiner

--- src/test/larp/parsercompiler/contextfreelanguage/ClosureLookaheadCombinerTest.java
+++ src/test/larp/parsercompiler/contextfreelanguage/ClosureLookaheadCombinerTest.java
@@ -10,35 +10,139 @@ package larp.parsercompiler.contextfreelanguage;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import larp.parsetree.contextfreelanguage.ConcatenationNode;
+import larp.parsetree.contextfreelanguage.DotNode;
+import larp.parsetree.contextfreelanguage.Node;
+import larp.parsetree.contextfreelanguage.NonTerminalNode;
+import larp.parsetree.contextfreelanguage.ProductionNode;
+import larp.parsetree.contextfreelanguage.TerminalNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class ClosureLookaheadCombinerTest
 {
     @Test
     public void testCombineReturnsEmptySetWhenClosureRulesSetIsEmpty()
     {
-        assertEquals(0, 1);
+        ClosureLookaheadCombiner combiner = new ClosureLookaheadCombiner();
+
+        Set<GrammarClosureRule> closureRules = new HashSet<GrammarClosureRule>();
+
+        Set<GrammarClosureRule> expectedClosureRules = new HashSet<GrammarClosureRule>();
+
+        assertEquals(expectedClosureRules, combiner.combine(closureRules));
     }
 
     @Test
-    public void testCombineReturnsInitialSetWheClosureRulesContainNoCommonProductions()
+    public void testCombineReturnsInitialSetWhenClosureRulesContainNoCommonProductions()
     {
-        assertEquals(0, 1);
+        ClosureLookaheadCombiner combiner = new ClosureLookaheadCombiner();
+
+        Set<GrammarClosureRule> closureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("a"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("B")));
+
+        Set<GrammarClosureRule> expectedClosureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> expectedLookaheadSymbols = new HashSet<Node>();
+        expectedLookaheadSymbols.add(new TerminalNode("a"));
+        expectedClosureRules.add(this.buildClosureRule(expectedLookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        expectedLookaheadSymbols = new HashSet<Node>();
+        expectedLookaheadSymbols.add(new TerminalNode("b"));
+        expectedClosureRules.add(this.buildClosureRule(expectedLookaheadSymbols, new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("B")));
+
+        assertEquals(expectedClosureRules, combiner.combine(closureRules));
     }
 
     @Test
     public void testCombineCombinesLookaheadsForClosureRulesWithSameProduction()
     {
-        assertEquals(0, 1);
+        ClosureLookaheadCombiner combiner = new ClosureLookaheadCombiner();
+
+        Set<GrammarClosureRule> closureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("a"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+
+        Set<GrammarClosureRule> expectedClosureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> expectedLookaheadSymbols = new HashSet<Node>();
+        expectedLookaheadSymbols.add(new TerminalNode("a"));
+        expectedLookaheadSymbols.add(new TerminalNode("b"));
+        expectedClosureRules.add(this.buildClosureRule(expectedLookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+
+        assertEquals(expectedClosureRules, combiner.combine(closureRules));
     }
 
     @Test
     public void testCombineRemovesRedundantClosureRules()
     {
-        assertEquals(0, 1);
+        ClosureLookaheadCombiner combiner = new ClosureLookaheadCombiner();
+
+        Set<GrammarClosureRule> closureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("a"));
+        lookaheadSymbols.add(new TerminalNode("b"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+
+        Set<GrammarClosureRule> expectedClosureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> expectedLookaheadSymbols = new HashSet<Node>();
+        expectedLookaheadSymbols.add(new TerminalNode("a"));
+        expectedLookaheadSymbols.add(new TerminalNode("b"));
+        expectedClosureRules.add(this.buildClosureRule(expectedLookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+
+        assertEquals(expectedClosureRules, combiner.combine(closureRules));
     }
 
     @Test
     public void testCombineCombinesOnlyClosureRulesWithSameProduction()
     {
-        assertEquals(0, 1);
+        ClosureLookaheadCombiner combiner = new ClosureLookaheadCombiner();
+
+        Set<GrammarClosureRule> closureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("a"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("b"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new TerminalNode("c"));
+        closureRules.add(this.buildClosureRule(lookaheadSymbols, new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("B")));
+
+        Set<GrammarClosureRule> expectedClosureRules = new HashSet<GrammarClosureRule>();
+        Set<Node> expectedLookaheadSymbols = new HashSet<Node>();
+        expectedLookaheadSymbols.add(new TerminalNode("a"));
+        expectedLookaheadSymbols.add(new TerminalNode("b"));
+        expectedClosureRules.add(this.buildClosureRule(expectedLookaheadSymbols, new NonTerminalNode("S"), new DotNode(), new NonTerminalNode("A")));
+        expectedLookaheadSymbols = new HashSet<Node>();
+        expectedLookaheadSymbols.add(new TerminalNode("c"));
+        expectedClosureRules.add(this.buildClosureRule(expectedLookaheadSymbols, new NonTerminalNode("A"), new DotNode(), new NonTerminalNode("B")));
+
+        assertEquals(expectedClosureRules, combiner.combine(closureRules));
+    }
+
+    private GrammarClosureRule buildClosureRule(Set<Node> lookaheadSymbols, NonTerminalNode nonTerminalNode, Node... rightHandNodes)
+    {
+        ProductionNode productionNode = new ProductionNode();
+        productionNode.addChild(nonTerminalNode);
+
+        ConcatenationNode concatenationNode = new ConcatenationNode();
+        for (Node rightHandNode: rightHandNodes)
+        {
+            concatenationNode.addChild(rightHandNode);
+        }
+        productionNode.addChild(concatenationNode);
+
+        return new GrammarClosureRule(productionNode, lookaheadSymbols);
     }
 }
