310dc1c1823b7dcaea5c26507b8a0725bbde2082
Brendan Rollinson <brendan.rollinson@gmail.com>
2019-10-07 19:55:59 -0400

    Creating LR1 parser compiler class

--- src/main/larp/parsercompiler/contextfreelanguage/LR1ParserCompiler.java
+++ src/main/larp/parsercompiler/contextfreelanguage/LR1ParserCompiler.java
@@ -7,13 +7,6 @@
 
 package larp.parsercompiler.contextfreelanguage;
 
-import org.junit.Test;
-
-public class LR1ParserCompilerTest
+public class LR1ParserCompiler
 {
-    @Test
-    public void testBogus()
-    {
-        LR1ParserCompiler compiler = new LR1ParserCompiler();
-    }
 }
