623cd8beb8567ff042c848b00850d9290177179b
Brendan Rollinson <brendan.rollinson@gmail.com>
2019-10-10 10:48:24 -0400

    Adding placeholder LR1 closure rule set DFA compiler class

---
+++ src/main/larp/parsercompiler/contextfreelanguage/LR1ClosureRuleSetDFACompiler.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2017 Brendan Rollinson-Lorimer
+ *
+ * This library is licensed under LGPL v2.1.
+ * See LICENSE.md for details.
+ */
+
+package larp.parsercompiler.contextfreelanguage;
+
+class LR1ClosureRuleSetDFACompiler
+{
+}
--- src/test/larp/parsercompiler/contextfreelanguage/LR1ClosureRuleSetDFACompilerTest.java
+++ src/test/larp/parsercompiler/contextfreelanguage/LR1ClosureRuleSetDFACompilerTest.java
@@ -7,6 +7,13 @@
 
 package larp.parsercompiler.contextfreelanguage;
 
+import org.junit.Test;
+
 public class LR1ClosureRuleSetDFACompilerTest
 {
+    @Test
+    public void testBogus()
+    {
+        LR1ClosureRuleSetDFACompiler compiler = new LR1ClosureRuleSetDFACompiler();
+    }
 }
