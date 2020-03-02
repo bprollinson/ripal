da5979e8f14af61f1a50cc6345fb796ac34d6672
Brendan Rollinson <brendan.rollinson@gmail.com>
2020-03-01 20:44:40 -0500

    First attempt at getting LR1 lookahead calculations enabled in DFA calculation

--- src/main/larp/parsercompiler/contextfreelanguage/LR1ClosureRuleSetDFACompiler.java
+++ src/main/larp/parsercompiler/contextfreelanguage/LR1ClosureRuleSetDFACompiler.java
@@ -7,6 +7,21 @@
 
 package larp.parsercompiler.contextfreelanguage;
 
+import larp.parsetree.contextfreelanguage.EndOfStringNode;
+import larp.parsetree.contextfreelanguage.Node;
+
+import java.util.HashSet;
+import java.util.Set;
+
 class LR1ClosureRuleSetDFACompiler extends LR0ClosureRuleSetDFACompiler
 {
+    protected Set<GrammarClosureRule> buildStartStateClosureRuleSet(Node firstProductionWithDot)
+    {
+        Set<Node> lookaheadSymbols = new HashSet<Node>();
+        lookaheadSymbols.add(new EndOfStringNode());
+        Set<GrammarClosureRule> closureRuleSet = new HashSet<GrammarClosureRule>();
+        closureRuleSet.add(new GrammarClosureRule(firstProductionWithDot, lookaheadSymbols));
+
+        return closureRuleSet;
+    }
 }
